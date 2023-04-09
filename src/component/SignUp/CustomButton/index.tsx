82a88b732ff1786407c9d0557021b8359f150431
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-09 15:30:34 +0900

    feat: 회원가입 페이지 커스텀 버튼 추가

---
+++ src/component/SignUp/CustomButton/index.tsx
@@ -0,0 +1,19 @@
+import styles from './CustomButton.module.scss';
+
+interface Button {
+  content:string,
+  type:string,
+  disable?: boolean
+}
+
+export default function CustomButton({ content, type, disable }:Button) {
+  return (
+    <button
+      type="button"
+      className={`${type} ${styles.button} ${type === 'large' ? styles.button__large : styles.button__small} ${disable ? styles.button__disable : null}`}
+    >
+      {content}
+
+    </button>
+  );
+}
