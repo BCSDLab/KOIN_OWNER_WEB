b8a9c634cef0f66a9a110720c3d49b2bc2602732
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-11 14:44:37 +0900

    feat: 회원가입 페이지 단계별 화면 렌더링 기능 추가

--- src/component/SignUp/CustomButton/index.tsx
+++ src/component/SignUp/CustomButton/index.tsx
@@ -3,19 +3,20 @@ import styles from './CustomButton.module.scss';
 interface Button {
   content:string,
   type:string,
-  disable?: boolean
+  disable?: boolean,
+  event?: () => void
 }
 
 export default function CustomButton({
-  content, type, disable,
+  content, type, disable, event,
 }:Button) {
   return (
     <button
       type="button"
       className={`${type} ${styles.button} ${type === 'large' ? styles.button__large : styles.button__small} ${disable ? styles.button__disable : null}`}
+      onClick={event}
     >
       {content}
-
     </button>
   );
 }
--- src/page/Auth/Signup/index.tsx
+++ src/page/Auth/Signup/index.tsx
@@ -1,3 +1,18 @@
+import CustomButton from 'component/SignUp/CustomButton';
+import { useState } from 'react';
+import OwnerData from './OwnerDataPage/OwnerData';
+import TermsOfService from './TermsOfServicePage';
+import UserData from './UserDataPage';
+import styles from './SignUp.module.scss';
+
 export default function Signup() {
-  return (<div>Signup test</div>);
+  const [step, setStep] = useState(0);
+  return (
+    <section className={styles.page_wrapper}>
+      {step === 0 && <TermsOfService />}
+      {step === 1 && <UserData />}
+      {step === 2 && <OwnerData />}
+      {step < 3 && <CustomButton type="large" disable content="다음" event={() => setStep(step + 1)} />}
+    </section>
+  );
 }
