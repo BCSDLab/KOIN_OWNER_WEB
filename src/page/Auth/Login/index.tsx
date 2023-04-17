b32599f28d062c90ee026bf8448161e25b7db607
hana <rlagksk8428@gmail.com>
2023-04-17 17:53:14 +0900

    feat: 로그인 placeholder  수정

--- src/page/Auth/Login/index.tsx
+++ src/page/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import cn from 'utils/ts/className';
+// import cn from 'utils/ts/className';
 import useBooleanState from 'utils/hooks/useBooleanState';
 import styles from './Login.module.scss';
 import { ReactComponent as Logo } from '../../../assets/svg/common/koin-logo.svg';
@@ -7,7 +7,6 @@ import { ReactComponent as BlindIcon } from '../../../assets/svg/auth/blind.svg'
 
 export default function Login() {
   const { value: isBlind, changeValue: changeIsBlind } = useBooleanState();
-  const { value: isNull, setTrue: setIsNull, setFalse: setIsNotNull } = useBooleanState();
 
   return (
     <div className={styles.template}>
@@ -19,15 +18,7 @@ export default function Login() {
               className={styles.form__input}
               type="text"
               placeholder="아이디 입력"
-              onChange={(e) => (e.target.value === '' ? setIsNull() : setIsNotNull())}
             />
-            <div className={cn({
-              [styles.form__text]: true,
-              [styles['form__text--null']]: isNull,
-            })}
-            >
-              @koreatech.ac.kr
-            </div>
           </div>
           <div className={styles.form__container}>
             <input
@@ -43,7 +34,16 @@ export default function Login() {
               {isBlind ? <ShowIcon aria-hidden /> : <BlindIcon aria-hidden />}
             </button>
           </div>
+          <div className={styles['auto-login']}>
+            <input className={styles['auto-login__checkbox']} type="checkbox" />
+            <div className={styles['auto-login__text']}>자동로그인</div>
+          </div>
           <button className={styles.form__button} type="submit">로그인</button>
+          <div className={styles.option}>
+            <div className={styles.option__text}>아이디 찾기</div>
+            <div className={styles.option__text}>비밀번호 찾기</div>
+            <div className={styles.option__text}>회원가입</div>
+          </div>
         </div>
       </div>
     </div>
