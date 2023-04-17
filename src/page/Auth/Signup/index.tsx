cca110ae2e23f158401dfd428c51fd8ecb741e6a
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-17 15:06:58 +0900

    feat: 회원가입 페이지 코인 로고 추가

--- src/page/Auth/Signup/index.tsx
+++ src/page/Auth/Signup/index.tsx
@@ -5,16 +5,25 @@ import TermsOfService from './TermsOfServicePage';
 import UserData from './UserDataPage';
 import styles from './SignUp.module.scss';
 import Complete from './CompletePage';
+import { ReactComponent as Logo } from '../../../assets/svg/common/koin-logo.svg';
 
 export default function Signup() {
   const [step, setStep] = useState(0);
   return (
-    <section className={styles.page}>
-      {step === 0 && <TermsOfService />}
-      {step === 1 && <UserData />}
-      {step === 2 && <OwnerData />}
+    <>
+      {step < 3 && (
+      <section className={styles['signup-section']}>
+        <Logo className={styles['signup-section__logo']} />
+        {step === 0 && <TermsOfService />}
+        {step === 1 && <UserData />}
+        {step === 2 && <OwnerData />}
+        <div className={styles['signup-section__next-button']}>
+          <CustomButton type="large" disable content="다음" event={() => setStep(step + 1)} />
+        </div>
+      </section>
+      )}
       {step === 3 && <Complete />}
-      {step < 3 && <CustomButton type="large" disable content="다음" event={() => setStep(step + 1)} />}
-    </section>
+    </>
+
   );
 }
