bb4068f3cd3d31e6996f421f7f56aa0362136c96
hana <rlagksk8428@gmail.com>
2023-04-17 20:26:45 +0900

    feat: 로그인 contents 가운데 정렬

--- src/layout/AuthLayout/index.tsx
+++ src/layout/AuthLayout/index.tsx
@@ -3,9 +3,9 @@ import styles from './AuthLayout.module.scss';
 
 export default function AuthLayout() {
   return (
-    <>
+    <div className={styles.template}>
       <Outlet />
       <div className={styles.copyright}> COPYRIGHT © 2023 BCSD LAB ALL RIGHTS RESERVED. </div>
-    </>
+    </div>
   );
 }
