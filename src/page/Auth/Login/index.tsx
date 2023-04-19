65928401900f274874e645fbcb3721769f4a9ba1
hana <rlagksk8428@gmail.com>
2023-04-19 16:14:06 +0900

    feat: 모바일버전 로그인 옵션 추가, 미디어쿼리 수정

--- src/page/Auth/Login/index.tsx
+++ src/page/Auth/Login/index.tsx
@@ -1,12 +1,17 @@
 // import cn from 'utils/ts/className';
 import useBooleanState from 'utils/hooks/useBooleanState';
+import cn from 'utils/ts/className';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import styles from './Login.module.scss';
 import { ReactComponent as Logo } from '../../../assets/svg/common/koin-logo.svg';
 import { ReactComponent as ShowIcon } from '../../../assets/svg/auth/show.svg';
 import { ReactComponent as BlindIcon } from '../../../assets/svg/auth/blind.svg';
+import { ReactComponent as LockIcon } from '../../../assets/svg/auth/lock.svg';
+import OPTION from './static/option';
 
 export default function Login() {
   const { value: isBlind, changeValue: changeIsBlind } = useBooleanState();
+  const { isMobile } = useMediaQuery();
 
   return (
     <div className={styles.template}>
@@ -17,14 +22,14 @@ export default function Login() {
             <input
               className={styles.form__input}
               type="text"
-              placeholder="아이디 입력"
+              placeholder={isMobile ? '이메일' : '아이디 입력'}
             />
           </div>
           <div className={styles.form__container}>
             <input
               className={styles.form__input}
               type={isBlind ? 'text' : 'password'}
-              placeholder="비밀번호 입력"
+              placeholder={isMobile ? '비밀번호' : '비밀번호 입력'}
             />
             <button
               type="button"
@@ -38,11 +43,31 @@ export default function Login() {
             <input className={styles['auto-login__checkbox']} type="checkbox" />
             <div className={styles['auto-login__text']}>자동로그인</div>
           </div>
-          <button className={styles.form__button} type="submit">로그인</button>
+          <button
+            className={cn({
+              [styles.form__button]: true,
+              [styles['form__button--mobile']]: true,
+            })}
+            type="submit"
+          >
+            로그인
+          </button>
+          { isMobile
+            ? (
+              <button className={styles.form__button} type="button">
+                회원가입
+              </button>
+            ) : null}
           <div className={styles.option}>
-            <div className={styles.option__text}>아이디 찾기</div>
-            <div className={styles.option__text}>비밀번호 찾기</div>
-            <div className={styles.option__text}>회원가입</div>
+            {isMobile
+              ? (
+                <>
+                  <LockIcon aria-hidden />
+                  <div className={styles.option__text}>{OPTION[1].name}</div>
+                </>
+              )
+              : OPTION.map((option) => (
+                <div className={styles.option__text}>{option.name}</div>))}
           </div>
         </div>
       </div>
