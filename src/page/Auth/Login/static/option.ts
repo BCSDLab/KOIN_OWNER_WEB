efca46d0ecbd84bd3cd7b050babc52f71a3a8b22
hana <rlagksk8428@gmail.com>
2023-04-19 16:14:27 +0900

    refactor: option 상수 분리

---
+++ src/page/Auth/Login/static/option.ts
@@ -0,0 +1,16 @@
+const OPTION = [
+  {
+    name: '아이디 찾기',
+    path: '',
+  },
+  {
+    name: '비밀번호 찾기',
+    path: '',
+  },
+  {
+    name: '회원가입',
+    path: '',
+  },
+] as const;
+
+export default OPTION;
