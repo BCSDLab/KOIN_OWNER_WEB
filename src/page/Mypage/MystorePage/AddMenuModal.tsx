b251abcf20c8258e1cbdfb70234e480544d955d1
dbshaejin <dbshaejin@gmail.com>
2023-04-20 14:50:13 +0900

    feat: 메뉴 추가 모달창 UI디자인

--- src/page/Mypage/MystorePage/AddMenuModal.tsx
+++ src/page/Mypage/MystorePage/AddMenuModal.tsx
@@ -10,11 +10,36 @@ export default function AddMenuModal({ closeModal }: Props) {
     <>
       <div className={styles.modalBack} onClick={closeModal} aria-hidden="true" />
       <div className={styles.modalContainer}>
-        <div className={styles.modalContainer__modalHeader}>
-          메뉴추가입니다~
+        <div className={styles.modalContainer__Header}>
+          <div className={styles.modalContainer__Title}>
+            메뉴 추가
+          </div>
         </div>
-        <div className={styles.modalContainer__new_menu}>
-          메뉴 들어가는 부분임
+        <div className={styles['modalContainer__new-setting']}>
+          <div className={styles['modalContainer__imgs-setting']}>
+            <div className={styles.modalContainer__settingtitle}>
+              메뉴사진
+            </div>
+            <div className={styles.modalContainer__caption}>
+              (최대 이미지 3장)
+            </div>
+          </div>
+          <div className={styles.modalContainer__blocks} />
+          <div className={styles.modalContainer__inputs}>
+            <div className={styles.modalContainer__titleinputs}>
+              <div className={styles.modalContainer__settingtitle}>
+                메뉴명
+              </div>
+              <input className={styles['modalContainer__menu-input']} placeholder="   예) 불족발 + 막국수 저녁 SET" />
+            </div>
+            <div className={styles.modalContainer__priceinputs}>
+              <div className={styles.modalContainer__settingtitle}>가격</div>
+              <div className={styles.modalContainer__info}>
+                <input className={styles['modalContainer__size-input']} placeholder="   예) 소 (1~2 인분)" />
+                <input className={styles['modalContainer__price-input']} placeholder="원   " />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
