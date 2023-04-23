22064a7af6b38354b22bb18a1484b80e14dc8085
dbshaejin <dbshaejin@gmail.com>
2023-04-23 22:33:22 +0900

    feat: 메뉴추가  모달창 아이콘 추가

--- src/page/Mypage/MystorePage/AddMenuModal.tsx
+++ src/page/Mypage/MystorePage/AddMenuModal.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as PlusIcon } from 'assets/svg/main/plus.svg';
+import { ReactComponent as GearIcon } from 'assets/svg/mystore/gear.svg';
 import styles from './MystorePage.module.scss';
 
 type Props = {
@@ -17,15 +19,17 @@ export default function AddMenuModal({ closeModal }: Props) {
         </div>
         <div className={styles['modalContainer__new-setting']}>
           <div className={styles['modalContainer__imgs-setting']}>
-            <div className={styles.modalContainer__settingtitle}>
-              메뉴사진
-            </div>
-            <div className={styles.modalContainer__caption}>
-              (최대 이미지 3장)
+            <div className={styles.modalContainer__captiontitle}>
+              <div className={styles.modalContainer__settingtitle}>
+                메뉴사진
+              </div>
+              <div className={styles.modalContainer__caption}>
+                (최대 이미지 3장)
+              </div>
             </div>
           </div>
           <div className={styles.modalContainer__blocks} />
-          <div className={styles.modalContainer__inputs}>
+          <div className={styles['modalContainer__info-setting']}>
             <div className={styles.modalContainer__titleinputs}>
               <div className={styles.modalContainer__settingtitle}>
                 메뉴명
@@ -38,7 +42,21 @@ export default function AddMenuModal({ closeModal }: Props) {
                 <input className={styles['modalContainer__size-input']} placeholder="   예) 소 (1~2 인분)" />
                 <input className={styles['modalContainer__price-input']} placeholder="원   " />
               </div>
+              <div className={styles.modalContainer__addPrice}>
+                <PlusIcon className={styles['modalContainer__addPrice-icon']} />
+                <div className={styles['modalContainer__addPrice-word']}>가격 추가</div>
+              </div>
             </div>
+            <div className={styles.modalContainer__captiontitle}>
+              <div className={styles.modalContainer__settingtitle}>
+                메뉴 카테고리
+              </div>
+              <div className={styles.modalContainer__caption}>
+                (최대 선택 n개)
+              </div>
+              <GearIcon className={styles['modalContainer__gear-icon']} />
+            </div>
+            <div className={styles.modalContainer__categorys} />
           </div>
         </div>
       </div>
