6f7b4213b5f187767a8d78af5bc2464b076ffa0b
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-24 20:54:40 +0900

    style: 약관 동의 페이지 모바일 뷰 스크롤 추가

--- src/page/Auth/Signup/TermsOfServicePage/index.tsx
+++ src/page/Auth/Signup/TermsOfServicePage/index.tsx
@@ -3,10 +3,10 @@ import styles from './TermsOfService.module.scss';
 
 export default function TermsOfService() {
   return (
-    <section className={styles['term--section']}>
+    <section className={styles['term-section']}>
       <div className={styles['agree--all']}>
         <label htmlFor="terms" className={styles['agree--all__content']}>
-          <input id="terms" type="checkbox" className={styles['term--section__checkbox']} />
+          <input id="terms" type="checkbox" className={styles['term-section__checkbox']} />
           <span className={styles['agree--all__pc--phrase']}>아래 이용약관에&nbsp;</span>
           모두 동의합니다.
         </label>
