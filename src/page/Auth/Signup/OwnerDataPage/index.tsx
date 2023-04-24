de9dcedf132d11abbc1ffbe9bd8ac3fe59e705e1
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-24 20:56:15 +0900

    feat: 사업장 정보 입력 페이지 모바일 뷰 추가

--- src/page/Auth/Signup/OwnerDataPage/index.tsx
+++ src/page/Auth/Signup/OwnerDataPage/index.tsx
@@ -1,43 +1,50 @@
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import styles from './OwnerData.module.scss';
 
 export default function OwnerData() {
+  const { isMobile } = useMediaQuery();
   return (
     <section className={styles.form}>
       <div>
         <span className={styles.form__label}>대표자명</span>
-        <input className={styles.form__input} type="text" />
+        <input className={styles.form__input} type="text" placeholder={isMobile ? '대표자명(실명)' : ''} />
       </div>
       <div>
         <span className={styles.form__label}>사업자등록번호</span>
-        <div className={styles['input--wrapper']}>
-          <div className={`${styles['form__input__registration--number']}`}>
-            <input className={`${styles['form__input__registration--number__first']}`} type="text" maxLength={3} />
+        {!isMobile ? (
+          <div className={styles['input-wrapper']}>
+            <div className={`${styles['form__input__registration-number']}`}>
+              <input className={`${styles['form__input__registration-number--first']}`} type="text" maxLength={3} />
+            </div>
+            <div className={`${styles['form__input__registration-number']}`}>
+              <input className={`${styles['form__input__registration-number--middle']}`} type="text" maxLength={2} />
+            </div>
+            <div>
+              <input className={`${styles['form__input__registration-number--last']}`} type="text" maxLength={5} />
+            </div>
           </div>
-          <div className={`${styles['form__input__registration--number']}`}>
-            <input className={`${styles['form__input__registration--number__middle']}`} type="text" maxLength={2} />
-          </div>
-          <div>
-            <input className={`${styles['form__input__registration--number__last']}`} type="text" maxLength={5} />
-          </div>
-        </div>
+        )
+          : <input className={styles.form__input} type="text" placeholder="사업자등록번호" />}
       </div>
       <div>
         <span className={styles.form__label}>파일첨부</span>
-        <div className={styles['file--box']}>
-          <div className={styles['file--box__plus--box--image']} />
-          <span className={styles['file--box__command']}>파일을 마우스로 끌어 오세요.</span>
-          <span className={styles['file--box__information']}>
-            사업자등록증, 영업신고증, 통장사본 이미지 필수
-            10mb 이하의 PDF 혹은 이미지 형식의
-            파일(e.g. jpg, png, gif 등)로 5개까지 업로드 가능합니다.
+        <div className={styles['file-box']}>
+          <div className={styles['file-box__plus-box-image']} />
+
+          <span className={styles['file-box__command']}>{!isMobile ? '파일을 마우스로 끌어 오세요.' : '파일을 첨부해주세요'}</span>
+          <span className={styles['file-box__information']}>
+            {!isMobile ? '사업자등록증, 영업신고증, 통장사본 이미지 필수10mb 이하의 PDF 혹은 이미지 형식의 파일(e.g. jpg, png, gif 등)로 5개까지 업로드 가능합니다.' : '사업자등록증, 영업신고증, 통장사본 이미지 첨부'}
           </span>
         </div>
-        <div className={styles['button--wrapper']}>
-          <label htmlFor="upload_button" className={styles['button--wrapper__upload--button']}>
+        {!isMobile && (
+        <div className={styles['button-wrapper']}>
+          <label htmlFor="upload_button" className={styles['button-wrapper__upload-button']}>
             <span>내 pc</span>
             <input id="upload_button" type="file" />
           </label>
         </div>
+        )}
+
       </div>
     </section>
   );
