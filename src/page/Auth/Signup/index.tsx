3df2571cf8dd26598a20fc0a20aca6b8c9460f9f
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-24 20:56:54 +0900

    feat: 회원가입 페이지 모바일 뷰 연결

--- src/page/Auth/Signup/index.tsx
+++ src/page/Auth/Signup/index.tsx
@@ -1,7 +1,9 @@
 import CustomButton from 'component/SignUp/CustomButton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 import { Link } from 'react-router-dom';
+import UserEmail from 'component/SignUp/UserEmail';
+import ProgressBar from 'component/SignUp/ProgressBar';
 import OwnerData from './OwnerDataPage';
 import TermsOfService from './TermsOfServicePage';
 import UserData from './UserDataPage';
@@ -11,49 +13,85 @@ import { ReactComponent as Logo } from '../../../assets/svg/common/koin-logo.svg
 
 export default function Signup() {
   const [step, setStep] = useState(0);
+
   const { isMobile } = useMediaQuery();
+  useEffect(() => {
+    setStep(0);
+  }, [isMobile]);
   return (
     <div className={styles['section-wrapper']}>
-      {step < 3 && (
-      <section className={styles['signup-section']}>
-        <div>
-          <Logo className={styles['signup-section__logo']} />
-          <span className={styles['signup-section__section-name']}>
-            사장님용
-            <br />
-            회원가입
-          </span>
-        </div>
-        {!isMobile ? (
+      {!isMobile
+        ? (
           <>
-            {step === 0 && <TermsOfService />}
-            {step === 1 && <UserData />}
-            {step === 2 && <OwnerData />}
-            <div className={styles['signup-section__next-button--pc']}>
-              <CustomButton type="large" disable content="다음" event={() => setStep(step + 1)} />
-            </div>
-          </>
-        ) : (
-          <>
-            {step === 0 && (
-            <>
-              <TermsOfService />
-              <div className={styles['signup-section__button-group--mobile']}>
-                <Link to="/login" className={styles['signup-section__button--mobile']}>취소</Link>
-                <CustomButton type="mobile" content="확인" event={() => setStep(step + 1)} />
+            {step < 3 && (
+            <section className={styles['signup-section']}>
+              <Logo className={styles['signup-section__logo']} />
+              <div>
+                {step === 0 && <TermsOfService />}
+                {step === 1 && <UserData />}
+                {step === 2 && <OwnerData />}
               </div>
-            </>
+              <div className={styles['signup-section__next-button--pc']}>
+                <CustomButton type="large" disable content="다음" event={() => setStep(step + 1)} />
+              </div>
+            </section>
             )}
-            {step === 1 && <UserData />}
-            {step === 2 && <OwnerData />}
-
+            {step === 3 && <Complete />}
+          </>
+        )
+        : (
+          <>
+            {step < 4 && (
+            <section className={styles['signup-section']}>
+              <span className={styles['signup-section__section-name']}>
+                사장님용
+                <br />
+                회원가입
+              </span>
+              <div>
+                {step === 0 && (
+                  <>
+                    <TermsOfService />
+                    <div className={styles['signup-section__button-group--mobile']}>
+                      <Link to="/login" className={styles['signup-section__button--mobile']}>취소</Link>
+                      <CustomButton type="mobile" content="확인" event={() => setStep(step + 1)} />
+                    </div>
+                  </>
+                )}
+                {step === 1 && (
+                  <>
+                    <ProgressBar step={step} />
+                    <UserData />
+                    <div className={styles['signup-section__button-group--mobile']}>
+                      <CustomButton type="large" content="이메일 인증하기" event={() => setStep(step + 1)} />
+                    </div>
+                  </>
+                )}
+                {step === 2 && (
+                  <>
+                    <ProgressBar step={step} />
+                    <UserEmail />
+                    <div className={styles['signup-section__button-group--mobile']}>
+                      <CustomButton type="mobile" content="재발송" event={() => { alert('재발송'); }} />
+                      <CustomButton type="mobile" content="다음" event={() => setStep(step + 1)} />
+                    </div>
+                  </>
+                )}
+                {step === 3 && (
+                  <>
+                    <ProgressBar step={step} />
+                    <OwnerData />
+                    <div className={styles['signup-section__button-group--mobile']}>
+                      <CustomButton type="large" content="다음" event={() => setStep(step + 1)} />
+                    </div>
+                  </>
+                )}
+              </div>
+            </section>
+            )}
+            {step === 4 && <Complete />}
           </>
         )}
-
-      </section>
-      )}
-      {step === 3 && <Complete />}
     </div>
-
   );
 }
