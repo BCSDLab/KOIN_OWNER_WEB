f99d2e8b44e1d2739fdd09900ae7c8a197bff59d
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-25 14:10:44 +0900

    feat: 가입완료 페이지 모바일 뷰 추가

--- src/page/Auth/Signup/CompletePage/index.tsx
+++ src/page/Auth/Signup/CompletePage/index.tsx
@@ -3,10 +3,10 @@ import styles from './Complete.module.scss';
 
 export default function Complete() {
   return (
-    <div className={styles.content_wrapper}>
-      <div className={styles.check_circle}><Check /></div>
+    <div className={styles['content-wrapper']}>
+      <div className={styles['complete-circle']}><Check className={styles['complete-circle--check-image']} /></div>
       <span className={styles.complete}>가입 신청 완료</span>
-      <div className={styles.complete_text}>
+      <div className={styles['complete-text']}>
         <span>
           가입 신청이 완료되었습니다.
         </span>
@@ -14,7 +14,7 @@ export default function Complete() {
           가입 허가가 승인되면 로그인이 가능합니다.
         </span>
       </div>
-      <div className={styles.link_button}>로그인 화면 바로가기</div>
+      <div className={styles['link-button']}>로그인 화면 바로가기</div>
     </div>
   );
 }
