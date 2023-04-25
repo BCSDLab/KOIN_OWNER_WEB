fa84234f273449ca5ecbf8cb67cb9c78641717b4
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-25 14:14:39 +0900

    feat: 가입 완료 페이지 로그인 페이지 돌아가기 버튼 라우팅 추가

--- src/page/Auth/Signup/CompletePage/index.tsx
+++ src/page/Auth/Signup/CompletePage/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { ReactComponent as Check } from '../../../../assets/svg/auth/check.svg';
 import styles from './Complete.module.scss';
 
@@ -14,7 +15,7 @@ export default function Complete() {
           가입 허가가 승인되면 로그인이 가능합니다.
         </span>
       </div>
-      <div className={styles['link-button']}>로그인 화면 바로가기</div>
+      <Link to="/login" className={styles['link-button']}>로그인 화면 바로가기</Link>
     </div>
   );
 }
