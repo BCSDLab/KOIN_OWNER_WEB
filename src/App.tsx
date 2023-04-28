1203c6b18fbdf978128dd9d3a32c97caeb5c8fc8
Kim Daeeui <kimeodml@gmail.com>
2023-04-28 20:09:11 +0900

    [공통] 헤더 추가 (#5)
    * feat: 메뉴 카테고리 추가
    
    * fix: 경로 수정
    
    * feat: 코인 로고 추가
    
    * feat: 헤더 추가
    
    * fix: Header 컴포넌트로 변경
    
    * feat: useMediaQuery 훅 추가
    
    * fix: useBooleanState 수정
    
    * feat: 모바일 버전 svg  추가
    
    * feat: 모바일 버전 헤더 추가
    
    * feat: stylelint 예외 적용
    
    * fix: svg 파일명 변경
    
    * fix: useBooleanState 반환값 객체로 변경
    
    * fix: 헤더 스타일 수정
    
    * feat: Home 폴더를 기본 경로로 설정
    
    * fix: 아이콘 변경
    
    * fix: 코드 오너 수정
    
    * style: font-family 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import DefaultLayout from 'layout/DefaultLayout';
 import Login from 'page/Auth/Login';
 import Signup from 'page/Auth/Signup';
+import Home from 'page/Home/index';
 import FindPassword from 'page/Auth/FindPassword';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/find-password" element={<FindPassword />} />
