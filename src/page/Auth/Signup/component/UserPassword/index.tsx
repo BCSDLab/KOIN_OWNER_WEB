71b2f63d0dafb5ab4aff0d58f1ac894fd774a8da
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-05-01 15:40:48 +0900

    feat: 비밀번호 입력 input 내의 비밀번호 가시화 태그 변경

--- src/page/Auth/Signup/component/UserPassword/index.tsx
+++ src/page/Auth/Signup/component/UserPassword/index.tsx
@@ -1,4 +1,5 @@
 import useMediaQuery from 'utils/hooks/useMediaQuery';
+import { ReactComponent as VisibleImage } from 'assets/svg/auth/visible.svg';
 import { ReactComponent as Warn } from 'assets/svg/auth/warning.svg';
 import styles from './UserPassword.module.scss';
 
@@ -9,12 +10,20 @@ export default function UserPassword() {
       {!isMobile && <span className={styles.form__label}>비밀번호</span>}
       <div className={styles['form__input--wrapper']}>
         <input className={styles.form__input} placeholder={`비밀번호${!isMobile ? ' 입력 (필수)' : ''}`} type="password" />
-        {!isMobile && <div className={styles['form__visible--button']} />}
+        {!isMobile && (
+        <button className={styles['form__visible--button']} type="button">
+          <VisibleImage />
+        </button>
+        )}
         <span className={styles.form__alert}>* 특수문자 포함 영어와 숫자 조합 6~18 자리</span>
       </div>
       <div className={styles['form__input--wrapper']}>
         <input className={styles.form__input} placeholder={`비밀번호 확인${!isMobile ? ' 입력 (필수)' : ''}`} type="password" />
-        {!isMobile && <div className={styles['form__visible--button']} />}
+        {!isMobile && (
+        <button className={styles['form__visible--button']} type="button">
+          <VisibleImage />
+        </button>
+        )}
         <div className={styles.form__warn}>
           <Warn />
           <span className={styles['form__warn--phrase']}>비밀번호가 일치하지 않습니다.</span>
