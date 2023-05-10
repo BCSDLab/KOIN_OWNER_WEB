016afed7a5f6a4e79a6509bcfcf1164116ea9297
Daeeui Kim <kimeodml@gmail.com>
2023-05-10 20:32:35 +0900

    feat: 이전 단계로 가는 버튼 추가

--- src/component/common/PreviousStep/index.tsx
+++ src/component/common/PreviousStep/index.tsx
@@ -1,10 +1,14 @@
 import { ReactComponent as PreviousArrowIcon } from 'assets/svg/common/mobile-back-arrow.svg';
 import styles from './PreviousStep.module.scss';
 
-export default function PreviousStep() {
+interface PreviousStepProps {
+  step: number;
+}
+
+export default function PreviousStep({ step }: PreviousStepProps) {
   return (
     <div className={styles.previous}>
-      <button type="button" className={styles.previous__button}>
+      <button type="button" className={styles.previous__button} disabled={step === 1}>
         <PreviousArrowIcon title="이전 단계로 가기" />
       </button>
     </div>
