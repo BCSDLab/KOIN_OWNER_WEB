2e2a516669e3ba048586ff40e11316e7391eca4b
hana <rlagksk8428@gmail.com>
2023-05-20 18:29:40 +0900

    feat: 로그인 파라미터 zod 스키마 적용

--- src/api/auth/model.ts
+++ src/api/auth/model.ts
@@ -1,9 +1,11 @@
 import z from 'zod';
 
-export interface LoginParam {
-  email: string;
-  password: string;
-}
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export type LoginSchemaType = z.infer<typeof LoginSchema>;
 
 export const LoginResponse = z.object({
   token: z.string(),
