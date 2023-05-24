76ac772e6e2052938e8c1fbdc5024d73dba7351a
Daeeui Kim <kimeodml@gmail.com>
2023-05-25 03:47:37 +0900

    feat: OperateTime 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import FindPassword from 'page/Auth/FindPassword';
 import AuthLayout from 'layout/AuthLayout';
 import MystorePage from 'page/MyStorePage';
 import StoreReg from 'page/Auth/StoreReg';
+import OperateTime from 'page/Auth/StoreReg/component/OperateTime';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/find-password" element={<FindPassword />} />
         <Route path="/store-registration" element={<StoreReg />} />
+        <Route path="/operate-time" element={<OperateTime />} />
       </Route>
     </Routes>
   );
