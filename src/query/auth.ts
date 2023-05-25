b3f9e3df919ed003efa7693af2432604f3ecb7cf
hana <rlagksk8428@gmail.com>
2023-05-25 21:21:29 +0900

    feat: useLogin refresh 토큰 로직 추가

--- src/query/auth.ts
+++ src/query/auth.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { getMe, postLogin } from 'api/auth';
+import { LoginClient } from 'model/auth';
 import { useNavigate } from 'react-router-dom';
 import useAuthStore from 'store/auth';
 
@@ -7,17 +8,24 @@ const useLogin = () => {
   const navigate = useNavigate();
   const setUser = useAuthStore((state) => state.setUser);
 
-  const { mutate, error, isError } = useMutation(postLogin, {
-    onSuccess: async ({ token }) => {
-      sessionStorage.setItem('token', token);
-      if (token) {
-        const authResponse = await getMe();
-        setUser(authResponse);
-      } else {
-        sessionStorage.removeItem('token');
+  const { mutate, error, isError } = useMutation({
+    mutationFn: (variables: LoginClient) => postLogin({
+      email: variables.email, password: variables.password,
+    }),
+    onSuccess: async (data, variables) => {
+      if (data.token) { localStorage.setItem('access_token', data.token); }
+      if (variables.isAutoLogin) {
+        localStorage.setItem('refresh_token', data.refresh_token);
       }
+      const authResponse = await getMe();
+      setUser(authResponse);
       navigate('/');
     },
+    onError: () => {
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+    },
+
   });
 
   return { mutate, error: error as { message: string }, isError };
