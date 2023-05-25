f477006561413ae4cb2d09063b43881760580688
hana <rlagksk8428@gmail.com>
2023-05-25 21:19:03 +0900

    feat: accessClient refresh token 인증 로직 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
 import API_PATH from 'config/constants';
+import { RefreshParams, RefreshResponse } from './auth/model';
 
 const client = axios.create({
   baseURL: `${API_PATH}`,
@@ -12,11 +14,45 @@ const accessClient = axios.create({
   timeout: 2000,
 });
 
-accessClient.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem('token');
-  config.headers.Authorization = `Bearer ${token}`;
+accessClient.interceptors.request.use(
+  async (config) => {
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
-  return config;
-});
+accessClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // accessToken만료시 새로운 accessToken으로 재요청 => 다시 getme
+    if (error.resopnse.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = localStorage.getItem('refresh_token');
+
+      if (refreshToken) {
+        client.post<RefreshParams, RefreshResponse>('/user/refresh', refreshToken)
+          .then(({ token }) => {
+            originalRequest.headers.authorization = `Bearer ${token}`;
+            localStorage.setItem('access_token', token);
+
+            return accessClient(originalRequest);
+          }).catch(() => {
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+
+            return Promise.reject(error);
+          });
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export { client, accessClient };
