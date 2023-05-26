ca6436f3f191dd96960cd152c7a2cfe39cbdc171
hana <rlagksk8428@gmail.com>
2023-05-26 15:04:20 +0900

    feat: 유저정보 저장 기능 추가

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -1,7 +1,20 @@
 import Header from 'component/common/Header';
-import { Outlet } from 'react-router-dom';
+import {
+  Outlet, redirect,
+} from 'react-router-dom';
+import { getMe } from 'api/auth';
+import useUserStore from 'store/user';
 
 export default function DefaultLayout() {
+  const setUser = useUserStore((state) => state.setUser);
+
+  const getUser = async () => {
+    const user = await getMe();
+    if (!user) redirect('/login');
+    setUser(user);
+  };
+  getUser();
+
   return (
     <>
       <Header />
