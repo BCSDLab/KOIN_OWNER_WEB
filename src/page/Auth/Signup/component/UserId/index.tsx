dd9139796c1c2231ead7c51972fad5acc90976ca
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-06-05 00:42:49 +0900

    feat: 비밀번호 중복확인 로직 추가

--- src/page/Auth/Signup/component/UserId/index.tsx
+++ src/page/Auth/Signup/component/UserId/index.tsx
@@ -1,10 +1,12 @@
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 import { ReactComponent as Warn } from 'assets/svg/auth/warning.svg';
 import CustomButton from 'page/Auth/Signup/component/CustomButton';
+import { useState } from 'react';
 import styles from './UserId.module.scss';
 
 export default function UserId() {
   const { isMobile } = useMediaQuery();
+  const [isUsable, setUsable] = useState<boolean | null>(null);
   return (
     !isMobile
       ? (
@@ -12,17 +14,36 @@ export default function UserId() {
           <span className={styles.form__label}>아이디</span>
           <div className={styles['input-block']}>
             <input className={styles['input-block__input']} id="id-input" placeholder="이메일 형식 아이디 입력(필수)" />
-            <CustomButton content="중복확인" buttonType="small" />
+            <CustomButton content="중복확인" buttonType="small" onClick={() => { setUsable(true); }} />
           </div>
-          <span className={styles.form__alert}>사용하실 수 있는 아이디 입니다.</span>
+          { isUsable !== null
+            && (
+            <>
+              {isUsable && <span className={styles.form__alert}>사용하실 수 있는 아이디 입니다.</span>}
+              {!isUsable && <span className={styles['form__warn--phrase']}>이미 가입된 이메일입니다.</span>}
+            </>
+            )}
         </div>
       )
       : (
         <div className={styles['input-block']}>
-          <input className={styles['input-block__input--warn']} id="id-input" placeholder="이메일" />
+          <div className={styles['input-block__input-elements']}>
+            <input className={styles[`${(isUsable !== null) ? 'input-block__input' : 'input-block__input--warn'}`]} id="id-input" placeholder="이메일" />
+            <CustomButton content="중복확인" buttonType="small" onClick={() => { setUsable(true); }} />
+          </div>
           <div className={styles.form__warn}>
-            <Warn />
-            <span className={styles['form__warn--phrase']}>이미 가입된 이메일입니다.</span>
+            { isUsable !== null
+            && (
+            <>
+              {isUsable && <span className={styles.form__alert}>사용하실 수 있는 아이디 입니다.</span>}
+              {!isUsable && (
+              <>
+                <Warn />
+                <span className={styles['form__warn--phrase']}>이미 가입된 이메일입니다.</span>
+              </>
+              )}
+            </>
+            )}
           </div>
         </div>
       )
