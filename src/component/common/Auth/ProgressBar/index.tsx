5493e05b510c8228d2592e5fc53075a0666a9c2b
Daeeui Kim <kimeodml@gmail.com>
2023-06-05 10:37:08 +0900

    feat: 프로그레스 바 컴포넌트로 분리

---
+++ src/component/common/Auth/ProgressBar/index.tsx
@@ -0,0 +1,26 @@
+import styles from './ProgressBar.module.scss';
+
+interface ProgressBarProps {
+  step: number;
+  total: number;
+  progressTitle: { step: number; title: string }[];
+}
+
+export default function ProgressBar({ step, total, progressTitle }: ProgressBarProps) {
+  const calculateWidth = () => ((step + 1) / total) * 100;
+
+  return (
+    <div className={styles.progress}>
+      <div className={styles.progress__title}>
+        <span>{`${progressTitle[step].step}. ${progressTitle[step].title}`}</span>
+        <span>{`${progressTitle[step].step} / ${total}`}</span>
+      </div>
+      <div className={styles.progress__bar}>
+        <span
+          className={styles['progress__bar--filled']}
+          style={{ width: `${calculateWidth()}%` }}
+        />
+      </div>
+    </div>
+  );
+}
