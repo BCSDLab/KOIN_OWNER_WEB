9bc6515a9e4d5b5ea90b2a20be9fb4b3c50d30ec
hana <rlagksk8428@gmail.com>
2023-06-06 20:23:06 +0900

    fix: handleErrorBoundary 로직 분리

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -3,23 +3,17 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import useUserStore from 'store/user';
 import useErrorBoundary from 'utils/hooks/useErrorBoundary';
 import useUser from 'utils/hooks/user';
-import { ZodError } from 'zod';
 
 export default function DefaultLayout() {
   const navigate = useNavigate();
   const user = useUserStore((state) => state.user);
   const { setUser } = useUser();
-  const { setError } = useErrorBoundary();
+  const { handleErrorBoundary } = useErrorBoundary();
 
   if (!user) {
     setUser()
-      .catch((error) => {
-        if (error instanceof ZodError) {
-          setError(error);
-        } else {
-          navigate('/login');
-        }
-      });
+      .catch(handleErrorBoundary)
+      .catch(() => navigate('/login'));
   }
 
   return (
