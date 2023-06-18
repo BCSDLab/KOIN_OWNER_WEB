f5c3963995929a87a7ff012139b9e4fb90954e5d
Daeeui Kim <kimeodml@gmail.com>
2023-06-18 15:23:04 +0900

    fix: progress 태그로 변경

--- src/component/common/Auth/ProgressBar/index.tsx
+++ src/component/common/Auth/ProgressBar/index.tsx
@@ -7,20 +7,13 @@ interface ProgressBarProps {
 }
 
 export default function ProgressBar({ step, total, progressTitle }: ProgressBarProps) {
-  const calculateWidth = ((step + 1) / total) * 100;
-
   return (
     <div className={styles.progress}>
       <div className={styles.progress__title}>
         <span>{`${progressTitle[step].step}. ${progressTitle[step].title}`}</span>
         <span>{`${progressTitle[step].step} / ${total}`}</span>
       </div>
-      <div className={styles.progress__bar}>
-        <span
-          className={styles['progress__bar--filled']}
-          style={{ width: `${calculateWidth}%` }}
-        />
-      </div>
+      <progress className={styles.progress__bar} value={step + 1} max={total} />
     </div>
   );
 }
