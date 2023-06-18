63ef9fd3b632f0d0ffc649d6676199eae8c64c2e
Daeeui Kim <kimeodml@gmail.com>
2023-06-18 14:00:02 +0900

    fix: 옵셔널 추가

--- src/component/common/Auth/SubTitle/index.tsx
+++ src/component/common/Auth/SubTitle/index.tsx
@@ -2,13 +2,13 @@ import styles from './SubTitle.module.scss';
 
 interface SubTitleProps {
   topTitle: string;
-  bottomTitle: string;
-  topText: string;
-  bottomText: string;
+  bottomTitle?: string;
+  topText?: string;
+  bottomText?: string;
 }
 
 export default function SubTitle({
-  topTitle, bottomTitle = '', topText = '', bottomText = '',
+  topTitle, bottomTitle, topText, bottomText,
 }: SubTitleProps) {
   return (
     <div className={styles.sub}>
