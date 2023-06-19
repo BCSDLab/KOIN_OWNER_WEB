72673462dccb720d95dc973b58deefd5c5d9c9c1
Daeeui Kim <kimeodml@gmail.com>
2023-06-20 01:38:25 +0900

    refactor: StoreRegistration 컴포넌트에만 Header 컴포넌트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,18 @@ import FindPassword from 'page/Auth/FindPassword';
 import AuthLayout from 'layout/AuthLayout';
 import MyStorePage from 'page/MyStorePage';
 import StoreRegistration from 'page/Auth/StoreRegistration';
-import OperateTime from 'page/Auth/StoreRegistration/component/OperateTime';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<MyStorePage />} />
+        <Route path="/store-registration" element={<StoreRegistration />} />
       </Route>
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/find-password" element={<FindPassword />} />
-        <Route path="/store-registration" element={<StoreRegistration />} />
-        <Route path="/operate-time" element={<OperateTime />} />
       </Route>
     </Routes>
   );
--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -1,7 +1,7 @@
 import ErrorBoundary from 'component/common/ErrorBoundary';
 import Header from 'component/common/Header';
 import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import usePrevPathStore from 'store/path';
 import useUserStore from 'store/user';
 import useErrorBoundary from 'utils/hooks/useErrorBoundary';
@@ -10,6 +10,7 @@ export default function DefaultLayout() {
   const navigate = useNavigate();
   const { user, setUser } = useUserStore((state) => state);
   const setPrevPath = usePrevPathStore((state) => state.setPrevPath);
+  const location = useLocation();
   const { handleErrorBoundary } = useErrorBoundary();
 
   useEffect(() => {
@@ -25,14 +26,12 @@ export default function DefaultLayout() {
 
   return (
     <div>
-      {user && (
-        <>
-          <Header />
-          <ErrorBoundary message="에러가 발생했습니다.">
-            <Outlet />
-          </ErrorBoundary>
-        </>
+      {user && location.pathname !== '/store-registration' && (
+        <Header />
       )}
+      <ErrorBoundary message="에러가 발생했습니다.">
+        <Outlet />
+      </ErrorBoundary>
     </div>
   );
 }
