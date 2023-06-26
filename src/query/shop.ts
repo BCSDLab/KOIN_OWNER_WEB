44caab1424a90d41031818474902c2f0faf9adc6
dbshaejin <dbshaejin@gmail.com>
2023-06-26 20:20:36 +0900

    fix: 네이밍 수정

--- src/query/shop.ts
+++ src/query/shop.ts
@@ -1,13 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMyShopList, getShopInfo, getMenuInfoList } from 'api/shop';
 
-// 스토어에 저장되어이쓴 정보를 가져온다.
 import useUserStore from 'store/user';
 
 const useMyShop = () => {
-  const userQueryKey = useUserStore.getState().user?.company_number;
+  const myShopQueryKey = useUserStore.getState().user?.company_number;
 
-  const { data: myShop } = useQuery(['myShop', userQueryKey], getMyShopList);
+  const { data: myShop } = useQuery(['myShop', myShopQueryKey], getMyShopList);
 
   const shopId = myShop?.shops[0].id;
 
