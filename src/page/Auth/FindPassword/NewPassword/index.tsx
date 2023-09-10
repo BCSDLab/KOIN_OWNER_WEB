3dada3db8a12f63d9b9ed33ad02a7ca660719368
MinGu Jeong <mingu830@gmail.com>
2023-09-11 00:31:20 +0900

    feat: 새 비밀번호 입력 UI 구현

---
+++ src/page/Auth/FindPassword/NewPassword/index.tsx
@@ -0,0 +1,40 @@
+// import { Outlet } from 'react-router-dom';
+import { ReactComponent as KoinLogo } from 'assets/svg/auth/koin-logo.svg';
+import { ReactComponent as Blind } from 'assets/svg/auth/blind.svg';
+import styles from './NewPassword.module.scss';
+
+export default function NewPassword() {
+  return (
+    <div className={styles.template}>
+      <KoinLogo className={styles.logo} />
+      <form className={styles.form}>
+        <label className={styles.form__label} htmlFor="new-password">
+          새 비밀번호
+          <div className={styles.input_container}>
+            <input
+              className={styles.form__input}
+              type="password"
+              id="new-password"
+              placeholder="비밀번호 입력 (필수)"
+            />
+            <Blind />
+          </div>
+        </label>
+        <span className={styles.password_tip}>* 특수문자 포함 영어와 숫자 조합 6~18자리</span>
+        <label className={styles.form__label} htmlFor="check-password">
+          비밀번호 확인
+          <div className={styles.input_container}>
+            <input
+              className={styles.form__input}
+              type="password"
+              id="check-password"
+              placeholder="비밀번호 확인 (필수)"
+            />
+            <Blind />
+          </div>
+        </label>
+        <button type="button" className={styles.submit}>다음</button>
+      </form>
+    </div>
+  );
+}
