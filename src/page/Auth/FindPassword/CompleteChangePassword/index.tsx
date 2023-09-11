71482bdab9cc0023a145a4b045a301f04507e397
MinGu Jeong <mingu830@gmail.com>
2023-09-11 17:03:57 +0900

    refactor: UI 구조 분리 명확화

--- src/page/Auth/FindPassword/CompleteChangePassword/index.tsx
+++ src/page/Auth/FindPassword/CompleteChangePassword/index.tsx
@@ -7,13 +7,15 @@ export default function CompleteChangePassword() {
       <div className={styles.circle}>
         <Check />
       </div>
-      <div className={styles.content__title}>비밀번호 변경 완료</div>
-      <div className={styles.content__description}>
-        비밀번호가 변경되었습니다.
-        <br />
-        새로운 비밀번호로 로그인 부탁드립니다.
+      <div className={styles.content}>
+        <div className={styles.content__title}>비밀번호 변경 완료</div>
+        <div className={styles.content__description}>
+          비밀번호가 변경되었습니다.
+          <br />
+          새로운 비밀번호로 로그인 부탁드립니다.
+        </div>
+        <button type="button" className={styles.content__button}>로그인 화면 바로가기</button>
       </div>
-      <button type="button" className={styles.content__button}>로그인 화면 바로가기</button>
     </div>
   );
 }
