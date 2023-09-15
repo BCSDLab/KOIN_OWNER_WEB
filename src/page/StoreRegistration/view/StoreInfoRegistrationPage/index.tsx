e4f45f2052bf80e44c3c5a56329930b81961c13b
dooohun <ehgns0305@naver.com>
2023-09-15 11:52:13 +0900

    chore: storeRegistration의 Complete 컴포넌트 삭제

--- src/page/StoreRegistration/view/StoreInfoRegistrationPage/index.tsx
+++ src/page/StoreRegistration/view/StoreInfoRegistrationPage/index.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import useStepStore from 'store/useStepStore';
 import Copyright from 'component/common/Copyright';
 import CustomButton from 'page/StoreRegistration/component/CustomButton';
-import Complete from 'page/StoreRegistration/component/Complete';
+import Complete from 'component/common/Auth/Complete';
 import InputBox from 'page/StoreRegistration/component/InputBox';
 import CategoryModal from 'page/StoreRegistration/modal/Category';
 import SearchStoreModal from 'page/StoreRegistration/modal/SearchStore';
@@ -41,84 +41,88 @@ export default function StoreInfo() {
   return (
     <>
       {step === 0 && (
-        <div className={styles.block}>
-          <Memo className={styles['block__writing-icon']} />
-          <span className={styles.block__title}>가게 정보 기입</span>
-          <div className={styles.block__text}>
-            <span>
-              가게의 다양한 정보를 입력 및 수정하여
-            </span>
-            <span>
-              학생들에게 최신 가게 정보를 알려주세요
-            </span>
+        <div className={styles.wrapper}>
+          <div className={styles.block}>
+            <Memo className={styles['block__writing-icon']} />
+            <span className={styles.block__title}>가게 정보 기입</span>
+            <div className={styles.block__text}>
+              <span>
+                가게의 다양한 정보를 입력 및 수정하여
+              </span>
+              <span>
+                학생들에게 최신 가게 정보를 알려주세요
+              </span>
+            </div>
+            <button
+              type="button"
+              onClick={() => setStep(step + 1)}
+              className={styles['block__next-button']}
+            >
+              가게 정보 기입
+            </button>
           </div>
-          <button
-            type="button"
-            onClick={() => setStep(step + 1)}
-            className={styles['block__next-button']}
-          >
-            가게 정보 기입
-          </button>
           <Copyright />
         </div>
       )}
       {step === 1 && (
-        <div className={styles.container}>
-          <Logo className={styles['container__koin-logo']} />
-          <form className={styles.form}>
-            <div>
-              <span className={styles.form__label}>대표 이미지</span>
-              <div className={styles['form__image-upload']}>
-                <Cutlery className={styles['form__cutlery-cross']} />
-                <span className={styles.form__title}>클릭하여 이미지를 등록해주세요.</span>
-              </div>
-            </div>
-            <div>
-              <span className={styles.form__label}>카테고리</span>
-              <div className={styles.form__section}>
-                <input type="text" className={styles.form__input} />
-                <CustomButton content="카테고리 검색" buttonType="small" modalId="categoryModal" onClick={toggleModal} />
-              </div>
-            </div>
-            <CategoryModal isOpen={isOpen.categoryModal} onClose={toggleModal} />
-            <InputBox content="대표자명" />
-            <div>
-              <span className={styles.form__label}>가게명</span>
-              <div className={styles.form__section}>
-                <input type="text" className={styles.form__input} />
-                <CustomButton content="가게검색" buttonType="small" modalId="searchStoreModal" onClick={toggleModal} />
-              </div>
-            </div>
-            <SearchStoreModal isOpen={isOpen.searchStoreModal} onClose={toggleModal} />
-            <InputBox content="주소정보" />
-            <InputBox content="전화번호" />
-            <InputBox content="배달금액" />
-            <div>
-              <span className={styles.form__label}>운영시간</span>
-              <div className={styles.form__section}>
-                <div className={styles['form__operate-time']}>
-                  <span>00:00 ~ 24:00</span>
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <Logo className={styles['container__koin-logo']} />
+            <form className={styles.form}>
+              <div>
+                <span className={styles.form__label}>대표 이미지</span>
+                <div className={styles['form__image-upload']}>
+                  <Cutlery className={styles['form__cutlery-cross']} />
+                  <span className={styles.form__title}>클릭하여 이미지를 등록해주세요.</span>
                 </div>
-                <CustomButton content="시간수정" buttonType="small" modalId="timeSettingModal" onClick={toggleModal} />
               </div>
-            </div>
-            <OperateTimeModal isOpen={isOpen.timeSettingModal} onClose={toggleModal} />
-            <InputBox content="기타정보" />
-            <div className={styles['form__next-button']}>
-              <CustomButton
-                content="다음"
-                buttonType="large"
-                modalId="confirmPopup"
-                onClick={toggleModal}
-              />
-            </div>
-            <ConfirmPopup isOpen={isOpen.confirmPopup} onClose={toggleModal} />
-          </form>
+              <div>
+                <span className={styles.form__label}>카테고리</span>
+                <div className={styles.form__section}>
+                  <input type="text" className={styles.form__input} />
+                  <CustomButton content="카테고리 검색" buttonType="small" modalId="categoryModal" onClick={toggleModal} />
+                </div>
+              </div>
+              <CategoryModal isOpen={isOpen.categoryModal} onClose={toggleModal} />
+              <InputBox content="대표자명" />
+              <div>
+                <span className={styles.form__label}>가게명</span>
+                <div className={styles.form__section}>
+                  <input type="text" className={styles.form__input} />
+                  <CustomButton content="가게검색" buttonType="small" modalId="searchStoreModal" onClick={toggleModal} />
+                </div>
+              </div>
+              <SearchStoreModal isOpen={isOpen.searchStoreModal} onClose={toggleModal} />
+              <InputBox content="주소정보" />
+              <InputBox content="전화번호" />
+              <InputBox content="배달금액" />
+              <div>
+                <span className={styles.form__label}>운영시간</span>
+                <div className={styles.form__section}>
+                  <div className={styles['form__operate-time']}>
+                    <span>00:00 ~ 24:00</span>
+                  </div>
+                  <CustomButton content="시간수정" buttonType="small" modalId="timeSettingModal" onClick={toggleModal} />
+                </div>
+              </div>
+              <OperateTimeModal isOpen={isOpen.timeSettingModal} onClose={toggleModal} />
+              <InputBox content="기타정보" />
+              <div className={styles['form__next-button']}>
+                <CustomButton
+                  content="다음"
+                  buttonType="large"
+                  modalId="confirmPopup"
+                  onClick={toggleModal}
+                />
+              </div>
+              <ConfirmPopup isOpen={isOpen.confirmPopup} onClose={toggleModal} />
+            </form>
+          </div>
           <Copyright />
         </div>
       )}
       {step === 2 && (
-        <div>
+        <div className={styles.wrapper}>
           <Complete title="가게 정보 등록 완료" topText="가게 등록이 완료되었습니다." bottomText="업체 정보 수정은 내 상점에서 가능합니다." link="/login" linkText="메인화면으로 바로가기" />
           <Copyright />
         </div>
