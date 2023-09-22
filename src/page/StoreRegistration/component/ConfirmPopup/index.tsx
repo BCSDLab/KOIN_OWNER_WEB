e931b985c67e81defda7ce708e6b455451fb0510
dooohun <ehgns0305@naver.com>
2023-09-22 18:15:02 +0900

    refactor: 클래스명 modal을 popup으로 변경

---
+++ src/page/StoreRegistration/component/ConfirmPopup/index.tsx
@@ -0,0 +1,41 @@
+import { createPortal } from 'react-dom';
+import useStepStore from 'store/useStepStore';
+import styles from './ConfirmPopup.module.scss';
+
+interface ConfirmPopupProps {
+  isOpen: boolean;
+  modalHandler: (event: React.MouseEvent) => void;
+}
+
+export default function ConfirmPopup({ isOpen, modalHandler }: ConfirmPopupProps) {
+  const { setStep } = useStepStore();
+
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div className={styles.popup}>
+      <div className={styles.content}>
+        <span className={styles['content__top-text']}>가게 정보를 저장하시겠습니까?</span>
+        <span className={styles['content__bottom-text']}>&apos;확인&apos;을 누르면 입력하신 내용으로 가게 정보가 등록됩니다.</span>
+        <div className={styles.content__buttons}>
+          <button
+            type="button"
+            onClick={modalHandler}
+            id="confirmPopup"
+            className={styles['content__cancel-button']}
+          >
+            취소
+          </button>
+          <button
+            type="button"
+            onClick={() => setStep(5)}
+            className={styles['content__next-button']}
+          >
+            확인
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.body,
+  );
+}
