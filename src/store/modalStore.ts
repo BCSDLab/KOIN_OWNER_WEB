68da4e845892cd4e0edc71f088d71245b3b18b6b
dooohun <ehgns0305@naver.com>
2023-10-31 23:06:10 +0900

    feat: 모달의 전역상태관리 변수 생성

---
+++ src/store/modalStore.ts
@@ -0,0 +1,40 @@
+import { create } from 'zustand';
+import WEEK from 'utils/constant/week';
+
+interface ModalStore {
+  categoryState: string | undefined,
+  searchStoreState: string | undefined,
+  openTimeState: { [key in typeof WEEK[number]]: string },
+  closeTimeState: { [key in typeof WEEK[number]]: string },
+  storeClosedState: { [key: string]: boolean },
+  setCategoryState: (state: string) => void,
+  setSearchStoreState: (state: string) => void,
+  setOpenTimeState: (state: { [key in typeof WEEK[number]]: string }) => void,
+  setCloseTimeState: (state: { [key in typeof WEEK[number]]: string }) => void,
+  setStoreClosedState: (state: { [key : string]: boolean }) => void,
+}
+
+const initialOperatingTime = {
+  월: '00:00',
+  화: '00:00',
+  수: '00:00',
+  목: '00:00',
+  금: '00:00',
+  토: '00:00',
+  일: '00:00',
+};
+
+const useModalStore = create<ModalStore>((set) => ({
+  categoryState: undefined,
+  searchStoreState: undefined,
+  openTimeState: initialOperatingTime,
+  closeTimeState: initialOperatingTime,
+  storeClosedState: { '': false },
+  setCategoryState: (state) => set({ categoryState: state }),
+  setSearchStoreState: (state) => set({ searchStoreState: state }),
+  setOpenTimeState: (state) => set(() => ({ openTimeState: state })),
+  setCloseTimeState: (state) => set(() => ({ closeTimeState: state })),
+  setStoreClosedState: (state) => set({ storeClosedState: state }),
+}));
+
+export default useModalStore;
