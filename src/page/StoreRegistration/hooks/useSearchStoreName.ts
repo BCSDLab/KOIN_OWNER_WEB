68b32f36298b33ec3b36433648e98d5cae1743fb
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-11-11 00:42:01 +0900

    feat: 가게 검색 기능 중 클릭 시 검색 기능 추가

---
+++ src/page/StoreRegistration/hooks/useSearchStoreName.ts
@@ -0,0 +1,21 @@
+import { useRef, useState } from 'react';
+
+export default function useSearchStoreName(stores :Array<{
+  name:string, tel: string, card:boolean, deliver:boolean, account:boolean
+}>) {
+  const [fillteredStores, setStores] = useState(stores);
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  const onClickSearchButton = (event:React.FormEvent) => {
+    event.preventDefault();
+    if (searchInput.current!.value !== '') {
+      setStores(stores.filter((
+        store,
+      ) => store.name.includes(searchInput.current!.value)));
+    } else {
+      setStores(stores);
+    }
+  };
+
+  return { searchInput, onClickSearchButton, fillteredStores };
+}
