604d97d0fc199fba6401d4d12ab28be7f1ed09db
MinGu Jeong <mingu830@gmail.com>
2023-11-11 02:39:00 +0900

    refactor: useRouteCheck Hook 가독성 향상

--- src/page/Auth/FindPassword/hooks/useRouteCheck.ts
+++ src/page/Auth/FindPassword/hooks/useRouteCheck.ts
@@ -1,20 +1,16 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-type RouteCheckValue = {
-  [key: string]: boolean
-};
-
-const useRouteCheck = (checkValue: string, route: string) => {
+const useRouteCheck = (prevState: string, entryRoute: string) => {
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const state = location.state as RouteCheckValue;
-    if (!state || !(checkValue in state)) {
-      navigate(route, { replace: true });
+    const 이전_상태를_가지고_넘어왔는가 = location.state && (prevState in location.state);
+    if (!이전_상태를_가지고_넘어왔는가) {
+      navigate(entryRoute, { replace: true });
     }
-  }, [location.state, navigate, route, checkValue]);
+  }, [location.state, navigate, entryRoute, prevState]);
 };
 
 export default useRouteCheck;
