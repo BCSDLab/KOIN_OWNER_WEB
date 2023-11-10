f405d9eae09f5db469f2f14713ec350019a0e677
MinGu Jeong <mingu830@gmail.com>
2023-11-10 20:31:42 +0900

    feat: 이메일 인증번호 발송, 검증 구현

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -15,3 +15,16 @@ export const getMe = async () => {
   const { data } = await accessClient.get<UserResponse>('/owner');
   return UserResponse.parse(data);
 };
+
+export const findPasswordVerify = ({ address }: { address: string }) => client.post('/owners/password/reset/verification', { address });
+
+export const findPassword = ({
+  address,
+  certificationCode,
+}: {
+  address: string;
+  certificationCode: string;
+}) => client.post('/owners/password/reset/send', {
+  address,
+  certification_code: certificationCode,
+});
--- src/page/Auth/FindPassword/SendAuthNumber/index.tsx
+++ src/page/Auth/FindPassword/SendAuthNumber/index.tsx
@@ -1,9 +1,29 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { ReactComponent as KoinLogo } from 'assets/svg/auth/koin-logo.svg';
+import { useState } from 'react';
 import cn from 'utils/ts/className';
+import { useMutation } from '@tanstack/react-query';
+import { findPassword, findPasswordVerify } from 'api/auth';
 import styles from './SendAuthNumber.module.scss';
 
 export default function FindPassword() {
+  const navigate = useNavigate();
+  const [emailInput, setEmailInput] = useState('');
+  const [verifyInput, setVerifyInput] = useState('');
+  const verifyEmail = useMutation({
+    mutationFn: () => findPasswordVerify({ address: emailInput }),
+  });
+
+  const submit = useMutation({
+    mutationFn: async () => findPassword({ address: emailInput, certificationCode: verifyInput }),
+    onSuccess: () => {
+      navigate('/new-password');
+    },
+    onError: () => {
+      // TODO: 이메일 인증 실패 시 UI 처리 필요
+    },
+  });
+
   return (
     <>
       <div className={styles.template}>
@@ -11,24 +31,41 @@ export default function FindPassword() {
         <form className={styles.form}>
           <label className={styles.form__label} htmlFor="email">
             이메일 입력
-            <input
-              className={styles.form__input}
-              type="text"
-              id="email"
-            />
+            <div className={styles['auth-container']}>
+              <input
+                className={cn({
+                  [styles.form__input]: true,
+                  [styles['form__input--auth']]: true,
+                })}
+                onChange={(e) => setEmailInput(e.target.value)}
+                type="text"
+                id="email"
+              />
+              <button type="button" className={styles['auth-button']} onClick={() => verifyEmail.mutate()} disabled={verifyEmail.isLoading}>
+                인증번호 발송
+              </button>
+            </div>
           </label>
           <label className={styles.form__label} htmlFor="auth-num">
             인증번호 보내기
-            <div className={styles['auth-container']}>
-              <input
-                className={cn({ [styles.form__input]: true, [styles['form__input--auth']]: true })}
-                type="text"
-                id="auth-num"
-              />
-              <button type="button" className={styles['auth-button']}>인증번호 발송</button>
-            </div>
+            <input
+              className={styles.form__input}
+              type="text"
+              id="auth-num"
+              onChange={(e) => setVerifyInput(e.target.value)}
+            />
           </label>
-          <button type="button" className={styles.submit}>다음</button>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              submit.mutate();
+            }}
+            className={styles.submit}
+            disabled={!verifyEmail.isSuccess}
+          >
+            다음
+          </button>
         </form>
       </div>
       <Outlet />
