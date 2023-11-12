caba70895e254d83712d1e7ffd06e1e2de7aa7d0
dooohun <ehgns0305@naver.com>
2023-11-12 21:05:27 +0900

    feat: 상점 api 추가

--- src/api/shop/index.ts
+++ src/api/shop/index.ts
@@ -1,6 +1,8 @@
 import { MyShopList, MyShopInfoRes, MyShopParam } from 'model/shopInfo/myShopInfo';
 import { MenuInfoRes } from 'model/shopInfo/menuCategory';
+import { AllShopList } from 'model/shopInfo/allShopInfo';
 import { accessClient } from 'api';
+import { OwnerShop } from 'model/shopInfo/ownerShop';
 
 export const getMyShopList = async () => {
   const { data } = await accessClient.get<MyShopList>('/owner/shops');
@@ -16,3 +18,10 @@ export const getMenuInfoList = async (param: MyShopParam) => {
   const { data } = await accessClient.get<MenuInfoRes>(`/owner/shops/${param.id}/menus`);
   return MenuInfoRes.parse(data);
 };
+
+export const getAllShopList = async () => {
+  const { data } = await accessClient.get<AllShopList>('/shops');
+  return AllShopList.parse(data);
+};
+
+export const postRegisterShop = (data: OwnerShop) => accessClient.post('/owner/shops', data);
