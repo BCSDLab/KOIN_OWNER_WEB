43c068d1e3119237112ba418b07d8d74f984f224
dooohun <ehgns0305@naver.com>
2023-11-12 21:24:31 +0900

    feat: InputBox 컴포넌트에서 입력값을 register함수를 사용하여 전달

--- src/page/StoreRegistration/component/InputBox/index.tsx
+++ src/page/StoreRegistration/component/InputBox/index.tsx
@@ -1,15 +1,26 @@
+import type { UseFormRegister } from 'react-hook-form';
+import { OwnerShop } from 'model/shopInfo/ownerShop';
 import styles from './InputBox.module.scss';
 
 interface InputProps {
   content: string;
-  id: string;
+  id: keyof OwnerShop
+  register: UseFormRegister<OwnerShop>;
+  isNumber?: boolean;
 }
 
-export default function InputBox({ content, id }: InputProps) {
+export default function InputBox({
+  content, id, register, isNumber,
+}: InputProps) {
   return (
     <label htmlFor={id} className={styles.form}>
       <span className={styles.form__label}>{content}</span>
-      <input type="text" id={id} className={styles.form__input} />
+      <input
+        type={isNumber ? 'number' : 'text'}
+        id={id}
+        className={styles.form__input}
+        {...register(id)}
+      />
     </label>
   );
 }
