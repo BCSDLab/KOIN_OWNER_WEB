dc8531f3d9f516817fbbee6ae639015c88f6b789
dooohun <ehgns0305@naver.com>
2023-11-23 16:25:42 +0900

    feat: 카테고리 섹션을 조작하기 위한 handleCategoryClick 함수 생성

--- src/page/ShopRegistration/component/Modal/Category/index.tsx
+++ src/page/ShopRegistration/component/Modal/Category/index.tsx
@@ -2,12 +2,18 @@ import useShopCategory from 'query/shopCategory';
 import { useState } from 'react';
 import cn from 'utils/ts/className';
 import useModalStore from 'store/modalStore';
+import { Category as CategoryProps } from 'model/category/storeCategory';
 import styles from './Category.module.scss';
 
 export default function Category() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const { categoryList } = useShopCategory();
   const { setCategoryState } = useModalStore();
+
+  const handleCategoryClick = (category: CategoryProps) => {
+    setSelectedCategory(category.name);
+    setCategoryState([category.name, category.id]);
+  };
 
   return (
     <div className={styles.category}>
@@ -18,10 +24,7 @@ export default function Category() {
             [styles['category__menu--selected']]: category.name === selectedCategory,
           })}
           type="button"
-          onClick={() => {
-            setSelectedCategory(category.name);
-            setCategoryState([category.name, category.id]);
-          }}
+          onClick={() => { handleCategoryClick(category); }}
           key={category.id}
         >
           <img className={styles.category__image} src={category.image_url} alt="" />
