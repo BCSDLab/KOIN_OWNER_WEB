3fa2df8227893a2f59882909ed9c9aa2c04ce89a
dooohun <ehgns0305@naver.com>
2023-12-22 16:49:39 +0900

    feat: 가게명, 주소를 전역 상태로 관리

--- src/page/ShopRegistration/view/Mobile/Main/index.tsx
+++ src/page/ShopRegistration/view/Mobile/Main/index.tsx
@@ -1,11 +1,13 @@
 import { ReactComponent as EmptyImgIcon } from 'assets/svg/shopRegistration/mobile-empty-img.svg';
 import useStepStore from 'store/useStepStore';
 import useImageUpload from 'utils/hooks/useImageUpload';
+import useShopRegistrationStore from 'store/shopRegistration';
 import styles from './Main.module.scss';
 
 export default function Main() {
   const { increaseStep } = useStepStore();
-  const { imgFile, imgRef, saveImgFile } = useImageUpload();
+  const { imageUrl, imgRef, saveImgFile } = useImageUpload();
+  const { setName, setAddress } = useShopRegistrationStore();
 
   return (
     <div className={styles.form}>
@@ -18,8 +20,8 @@ export default function Main() {
           onChange={saveImgFile}
           ref={imgRef}
         />
-        {imgFile
-          ? <img src={imgFile} className={styles['form__main-menu']} alt="" />
+        {imageUrl
+          ? <img src={imageUrl} className={styles['form__main-menu']} alt="" />
           : (
             <>
               <EmptyImgIcon />
@@ -29,11 +31,21 @@ export default function Main() {
       </label>
       <label htmlFor="name" className={styles.form__label}>
         가게명
-        <input type="text" id="name" className={styles.form__input} />
+        <input
+          type="text"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          className={styles.form__input}
+        />
       </label>
       <label htmlFor="address" className={styles.form__label}>
         주소정보
-        <input type="text" id="address" className={styles.form__input} />
+        <input
+          type="text"
+          id="address"
+          onChange={(e) => setAddress(e.target.value)}
+          className={styles.form__input}
+        />
       </label>
       <div className={styles.form__button}>
         <button type="button" onClick={increaseStep}>다음</button>
