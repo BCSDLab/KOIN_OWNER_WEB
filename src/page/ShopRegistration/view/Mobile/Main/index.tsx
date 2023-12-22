3c5b982a8ed446ab4170a9f3294409d23b6a195f
dooohun <ehgns0305@naver.com>
2023-12-22 16:21:53 +0900

    feat: 모바일 사진 등록 로직 추가

--- src/page/ShopRegistration/view/Mobile/Main/index.tsx
+++ src/page/ShopRegistration/view/Mobile/Main/index.tsx
@@ -1,16 +1,32 @@
 import { ReactComponent as EmptyImgIcon } from 'assets/svg/shopRegistration/mobile-empty-img.svg';
 import useStepStore from 'store/useStepStore';
+import useImageUpload from 'utils/hooks/useImageUpload';
 import styles from './Main.module.scss';
 
 export default function Main() {
   const { increaseStep } = useStepStore();
+  const { imgFile, imgRef, saveImgFile } = useImageUpload();
 
   return (
     <div className={styles.form}>
-      <div className={styles.form__img}>
-        <EmptyImgIcon />
-        <div>등록된 이미지가 없습니다.</div>
-      </div>
+      <label className={styles['form__image-upload']} htmlFor="mainMenuImage">
+        <input
+          type="file"
+          accept="image/*"
+          id="mainMenuImage"
+          className={styles['form__upload-file']}
+          onChange={saveImgFile}
+          ref={imgRef}
+        />
+        {imgFile
+          ? <img src={imgFile} className={styles['form__main-menu']} alt="" />
+          : (
+            <>
+              <EmptyImgIcon />
+              <span className={styles.form__text}>등록된 이미지가 없습니다.</span>
+            </>
+          )}
+      </label>
       <label htmlFor="name" className={styles.form__label}>
         가게명
         <input type="text" id="name" className={styles.form__input} />
