ae7ace099ee3db888964fba2f1f72a7b319faa06
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2024-01-02 15:40:43 +0900

    refactor: CustomButton 사이즈 주석 추가

--- src/page/Auth/Signup/component/CustomButton/index.tsx
+++ src/page/Auth/Signup/component/CustomButton/index.tsx
@@ -2,6 +2,7 @@ import styles from './CustomButton.module.scss';
 
 interface ButtonProps {
   content:string,
+  // 80px | 부모 요소의 절반 width | 부모요소 전체 width | 83px
   buttonSize: 'small' | 'mobile' | 'large' | 'mobile-small',
   disable?: boolean,
   submit?:boolean
