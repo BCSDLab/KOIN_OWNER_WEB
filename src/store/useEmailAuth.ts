c326527b4fc5531118ba3f4de27dbcc636119c26
MinGu Jeong <mingu830@gmail.com>
2024-01-03 05:07:13 +0900

    feat: 이메일값이 세션스토리지에 저장되는 기능 구현

--- src/store/useEmailAuth.ts
+++ src/store/useEmailAuth.ts
@@ -1,13 +1,17 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface EmailAuth {
   email: string;
   setEmail: (email: string) => void;
 }
 
-const useEmailAuthStore = create<EmailAuth>((set) => ({
+const useEmailAuthStore = create(persist<EmailAuth>((set) => ({
   email: '',
   setEmail: (email: string) => set({ email }),
+}), {
+  name: 'email-storage',
+  getStorage: () => sessionStorage,
 }));
 
 export default useEmailAuthStore;
