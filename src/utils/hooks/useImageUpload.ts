feafb1a969357658bb4eeba6d300c0f66127d230
dooohun <ehgns0305@naver.com>
2024-01-10 22:09:34 +0900

    fix: 가게 등록 시 이미지가 업로드 되지 않는 현상 해결

--- src/utils/hooks/useImageUpload.ts
+++ src/utils/hooks/useImageUpload.ts
@@ -1,23 +1,35 @@
+import { uploadFile } from 'api/uploadFile/Uploadfile';
 import { useRef, useState } from 'react';
 
 export default function useImageUpload() {
   const [imageFile, setImageFile] = useState('');
+  const [isUploadError, setIsUploadError] = useState(false);
   const imgRef = useRef<HTMLInputElement>(null);
 
-  const saveImgFile = () => {
+  const saveImgFile = async () => {
     const file = imgRef.current?.files?.[0];
     const reader = new FileReader();
 
-    reader.onloadend = () => {
-      const { result } = reader;
-      if (typeof result === 'string') {
-        setImageFile(result);
-      }
-    };
     if (file) {
+      const formData = new FormData();
+      formData.append('multipartFile', file);
+      try {
+        const data = await uploadFile(formData);
+
+        if (data?.data?.file_url) {
+          setImageFile(`https://${data.data.file_url}`);
+        }
+      } catch (error) {
+        if (error) {
+          setIsUploadError(true);
+        }
+      }
+      setIsUploadError(false);
       reader.readAsDataURL(file);
     }
   };
 
-  return { imageFile, imgRef, saveImgFile };
+  return {
+    imageFile, imgRef, saveImgFile, isUploadError,
+  };
 }
