2fa37173fba463bc8a94b5ac1a56f1d9e04da785
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2024-01-12 15:48:44 +0900

    fix: 에러 메시지가 1개일 경우의 ErrorMessage 컴포넌트 props 추가

--- src/page/Auth/Signup/component/ErrorMessage/index.tsx
+++ src/page/Auth/Signup/component/ErrorMessage/index.tsx
@@ -2,14 +2,16 @@ import { ReactComponent as Warn } from 'assets/svg/auth/warning.svg';
 import styles from './ErrorMessage.module.scss';
 
 interface ErrorMessageProps {
-  messages: (string | undefined)[]
+  messages?: (string | undefined)[]
+  message?:string
 }
 
-export default function ErrorMessage({ messages }:ErrorMessageProps) {
+export default function ErrorMessage({ messages, message }:ErrorMessageProps) {
   return (
     <div className={styles.warn}>
       <Warn />
-      <span className={styles['warn--phrase']}>{messages[0]}</span>
+      {message ? <span className={styles['warn--phrase']}>{message}</span>
+        : messages && <span className={styles['warn--phrase']}>{messages[0]}</span>}
     </div>
   );
 }
