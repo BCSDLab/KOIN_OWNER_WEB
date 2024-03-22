3ccc2d0a02a8982e9c93969fabdd7252751dd4b5
dooohun <ehgns0305@naver.com>
2024-03-22 19:28:14 +0900

    fix: https 삭제

--- src/page/Auth/Signup/utils/parseRegisterData.ts
+++ src/page/Auth/Signup/utils/parseRegisterData.ts
@@ -11,7 +11,7 @@ const parseRegisterData = async (
 ) => {
   const companyNumber = ownerInfo.registrationNumberMobile ? ownerInfo.registrationNumberMobile.replace(/^(\d{3})(\d{2})(\d+)/, '$1-$2-$3') : `${ownerInfo.registrationNumberFront}-${ownerInfo.registrationNumberMiddle}-${ownerInfo.registrationNumberEnd}`;
   const phoneNumber = ownerInfo.phoneMobile ? ownerInfo.phoneMobile.replace(/^(\d{3})(\d{4})(\d+)/, '$1-$2-$3') : `${ownerInfo.phoneFront}-${ownerInfo.phoneMiddle}-${ownerInfo.phoneEnd}`;
-  const attachmentUrls = fileUrls!.map((file) => ({ file_url: `https://${file}` }));
+  const attachmentUrls = fileUrls!.map((file) => ({ file_url: `${file}` }));
   const shopId = ownerInfo.shopName === searchShopState ? Number(selectedShopId) : null;
   const hashedPassword = await sha256(userInfo.password!);
 
