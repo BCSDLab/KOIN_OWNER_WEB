0e9ba67f556bd19f0f31bf7c8b06bc3013d76608
MinGu Jeong <mingu830@gmail.com>
2024-04-04 16:40:03 +0900

    feat: 웹에서 center 정렬

--- src/page/Coop/index.tsx
+++ src/page/Coop/index.tsx
@@ -7,9 +7,11 @@ import styles from './Coop.module.scss';
 export default function Coop() {
   const [selectedMenuType, setSelectedMenuType] = useState<Menus>('아침');
   return (
-    <div className={styles.container}>
-      <MenuType selectedMenuType={selectedMenuType} setSelectedMenuType={setSelectedMenuType} />
-      <MenuCard selectedMenuType={selectedMenuType} />
+    <div className={styles['container-wrapper']}>
+      <div className={styles.container}>
+        <MenuType selectedMenuType={selectedMenuType} setSelectedMenuType={setSelectedMenuType} />
+        <MenuCard selectedMenuType={selectedMenuType} />
+      </div>
     </div>
   );
 }
