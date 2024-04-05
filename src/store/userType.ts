861cba66771a37277c8b166afd03fbc63aa85bf6
정해성 <gotjd0230@naver.com>
2024-04-05 09:45:07 +0900

    feat: isAuth 추가

--- src/store/userType.ts
+++ src/store/userType.ts
@@ -3,12 +3,16 @@ import { create } from 'zustand';
 
 interface UserTypeStore {
   userType: UserType;
+  isAuth: boolean;
   setUserType: (type: UserType) => void;
+  setIsAuth: (auth: boolean) => void;
 }
 
 const useUserTypeStore = create<UserTypeStore>((set) => ({
   userType: 'NOT_LOGGED_IN',
+  isAuth: false,
   setUserType: (userType) => set({ userType }),
+  setIsAuth: (isAuth) => set({ isAuth }),
 }));
 
 export default useUserTypeStore;
