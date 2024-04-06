37ddda0888b5ca0405eef458897cf144be4564cc
MinGu Jeong <mingu830@gmail.com>
2024-04-07 03:20:40 +0900

    feat: 현재 시각에 따른 MenuType 자동변경 구현

--- src/page/Coop/index.tsx
+++ src/page/Coop/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import { Menus } from 'model/Coop';
+import getCurrentMenuType from 'utils/hooks/useGetCurrentMenyType';
 import MenuCard from './components/MenuCard';
 import MenuType from './components/MenuType';
 import styles from './Coop.module.scss';
 
 export default function Coop() {
-  const [selectedMenuType, setSelectedMenuType] = useState<Menus>('아침');
+  const [selectedMenuType, setSelectedMenuType] = useState<Menus>(getCurrentMenuType());
   return (
     <div className={styles['container-wrapper']}>
       <div className={styles.container}>
---
+++ src/utils/hooks/useGetCurrentMenyType.ts
@@ -0,0 +1,21 @@
+import { Menus } from 'model/Coop';
+
+const getCurrentMenuType = (): Menus => {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const time = hour * 60 + minute;
+
+  // 00:01~10:30
+  if (time >= 1 && time <= 630) {
+    return '아침';
+  }
+  // 10:31~15:00
+  if (time <= 900) {
+    return '점심';
+  }
+  // 15:01~23:59
+  return '저녁';
+};
+
+export default getCurrentMenuType;
