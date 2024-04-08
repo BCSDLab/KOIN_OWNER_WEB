3f25baa4dcdce830215ff3f12a6526917f36cf41
MinGu Jeong <mingu830@gmail.com>
2024-04-09 05:22:44 +0900

    feat: 영양사 식단 이미지 업로드 API

--- src/api/uploadFile/Uploadfile.ts
+++ src/api/uploadFile/Uploadfile.ts
@@ -1,5 +1,17 @@
 import { accessClient } from 'api';
 
+interface File {
+  content_length: number;
+  content_type: string;
+  file_name: string;
+}
+
+export interface FileResponse {
+  pre_signed_url: string;
+  file_name: string;
+  expiration_date: string;
+}
+
 export const uploadFile = (formData: FormData) => accessClient.post('/OWNERS/upload/file', formData, {
   headers: { 'Content-Type': 'multipart/form-data' },
 }).catch((error) => Promise.reject(error));
@@ -7,3 +19,5 @@ export const uploadFile = (formData: FormData) => accessClient.post('/OWNERS/upl
 export const uploadFiles = (formData: FormData) => accessClient.post('/OWNERS/upload/files', formData, {
   headers: { 'Content-Type': 'multipart/form-data' },
 });
+
+export const getCoopUrl = (fileName: File) => accessClient.post<FileResponse>('/coop/upload/url', fileName);
