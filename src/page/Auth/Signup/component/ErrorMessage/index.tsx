654f7f1437c1a70e17c5a1cf492a27229f3a0131
최원빈 <wonbeen@tossbank.com>
2024-04-10 21:32:29 +0900

    fix: 에러를 쓰고있는쪽에서 변경된 error interface에 맞지 않는 문제 대응

--- src/page/Auth/Signup/component/ErrorMessage/index.tsx
+++ src/page/Auth/Signup/component/ErrorMessage/index.tsx
@@ -2,16 +2,31 @@ import { ReactComponent as Warn } from 'assets/svg/auth/warning.svg';
 import styles from './ErrorMessage.module.scss';
 
 interface ErrorMessageProps {
-  message:string | (string | undefined)[]
+  message: string | (string | undefined | null)[]
 }
 
 export default function ErrorMessage({ message }:ErrorMessageProps) {
-  return (
-    <div className={styles.warn}>
-      <Warn />
-      <span className={styles['warn--phrase']}>
-        {typeof message === 'string' ? message : message[0]}
-      </span>
-    </div>
-  );
+  if (typeof message === 'string') {
+    return (
+      <div className={styles.warn}>
+        <Warn />
+        <span className={styles['warn--phrase']}>
+          {message}
+        </span>
+      </div>
+    );
+  }
+
+  if (message.length > 0) {
+    return (
+      <div className={styles.warn}>
+        <Warn />
+        <span className={styles['warn--phrase']}>
+          {typeof message === 'string' ? message : message[0]}
+        </span>
+      </div>
+    );
+  }
+
+  return null;
 }
