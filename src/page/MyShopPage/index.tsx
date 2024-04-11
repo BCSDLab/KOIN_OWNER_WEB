eb8db12b571bed40bae18b7e8e680be2d8e21386
chaeseungyun <thslr57@naver.com>
2024-04-11 14:49:27 +0900

    refactor: 테스트 진입점 삭제

--- src/page/MyShopPage/index.tsx
+++ src/page/MyShopPage/index.tsx
@@ -63,7 +63,6 @@ export default function MyShopPage() {
         <>
           <div className={styles.mobileheader}>
             <h1 className={styles.mobileheader__title}>가게정보</h1>
-            {shopData && <Link to={`/owner/event-add/${shopData.id}`}>이벤트 추가</Link>}
             <Link to="/owner/add-menu">
               <button
                 type="button"
