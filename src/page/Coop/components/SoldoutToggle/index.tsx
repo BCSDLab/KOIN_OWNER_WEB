3d1b6d49bc6228f27ce2bc23a3ae05436c4775db
MinGu Jeong <mingu830@gmail.com>
2024-04-15 03:32:08 +0900

    refactor: 토글버튼 사파리대응

--- src/page/Coop/components/SoldoutToggle/index.tsx
+++ src/page/Coop/components/SoldoutToggle/index.tsx
@@ -19,8 +19,23 @@ export default function SoldoutToggle({ menuId, onClick, menu }: SoldoutTogglePr
 
   return (
     <svg className={styles['toggle-button']} onClick={handleToggle} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 23">
-      <rect className={styles.background} x="1.5" y="2" width="43" height="19" fill={isActive ? '#175C8E' : '#E1E1E1'} stroke="#F5F5F5" />
-      <circle className={styles.circle} cx={isActive ? 35 : 11} cy="11.5" />
+      <rect
+        className={styles.background}
+        x="1.5"
+        y="2"
+        width="43"
+        height="19"
+        rx="9.5"
+        fill={isActive ? '#175C8E' : '#E1E1E1'}
+        stroke="#F5F5F5"
+        strokeWidth="3"
+      />
+      <circle
+        cx={isActive ? 35 : 11}
+        cy="11.5"
+        r="8"
+        fill="white"
+      />
     </svg>
   );
 }
