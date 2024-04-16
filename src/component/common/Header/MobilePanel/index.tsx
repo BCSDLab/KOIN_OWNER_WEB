738b8214584a5fcdbcd0777ca16b68d7d79643bf
dooohun <ehgns0305@naver.com>
2024-04-16 16:50:21 +0900

    feat: suspense 추가

--- src/component/common/Header/MobilePanel/index.tsx
+++ src/component/common/Header/MobilePanel/index.tsx
@@ -10,6 +10,7 @@ import { useLogout } from 'query/auth';
 import usePrevPathStore from 'store/path';
 import useMobileSidebar from 'component/common/Header/hooks/useMobileSidebar';
 import useSuspenseUser from 'utils/hooks/useSuspenseUser';
+import { Suspense } from 'react';
 import styles from './MobilePanel.module.scss';
 
 export default function MobilePanel() {
@@ -65,80 +66,82 @@ export default function MobilePanel() {
       </div>
       {createPortal(
         (
-          <nav className={cn({
-            [styles['mobile-header__panel']]: true,
-            [styles['mobile-header__panel--logged-in']]: true,
-            [styles['mobile-header__panel--show']]: isMobileSidebarExpanded,
-          })}
-          >
-            <div className={styles['mobile-header__user']}>
-              <button
-                title="뒤로 가기 버튼"
-                className={styles['mobile-header__backspace']}
-                type="button"
-                onClick={hideSidebar}
-              >
-                <BackArrowIcon title="뒤로 가기 버튼" />
-              </button>
-              <div className={styles['mobile-header__greet']}>
-                {user?.name}
-                <span>님, 안녕하세요!</span>
-              </div>
-              <ul className={styles['mobile-header__auth-menu']}>
-                <li className={styles['mobile-header__my-info']}>
-                  <Link to="/owner/modify-info">
-                    내 정보
-                  </Link>
-                </li>
-                <li className={styles['mobile-header__link']}>
-                  <button type="button" onClick={handleLogout}>
-                    로그아웃
-                  </button>
-                </li>
-              </ul>
-            </div>
-
-            {CATEGORY.map((categoryInfo) => (
-              <div key={categoryInfo.title}>
-                <div>
-                  <div className={styles['mobile-header__category-title']}>
-                    {categoryInfo.title}
-                  </div>
-                  <ul className={styles['mobile-header__sub-menus']}>
-                    {categoryInfo.submenu.map((subMenu) => (
-                      <li
-                        className={styles['mobile-header__sub-menu']}
-                        key={subMenu.title}
-                      >
-                        <Link to={subMenu.link}>
-                          {subMenu.title === '가게정보' && subMenu.title}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+          <Suspense fallback={<div />}>
+            <nav className={cn({
+              [styles['mobile-header__panel']]: true,
+              [styles['mobile-header__panel--logged-in']]: true,
+              [styles['mobile-header__panel--show']]: isMobileSidebarExpanded,
+            })}
+            >
+              <div className={styles['mobile-header__user']}>
+                <button
+                  title="뒤로 가기 버튼"
+                  className={styles['mobile-header__backspace']}
+                  type="button"
+                  onClick={hideSidebar}
+                >
+                  <BackArrowIcon title="뒤로 가기 버튼" />
+                </button>
+                <div className={styles['mobile-header__greet']}>
+                  {user?.name}
+                  <span>님, 안녕하세요!</span>
                 </div>
+                <ul className={styles['mobile-header__auth-menu']}>
+                  <li className={styles['mobile-header__my-info']}>
+                    <Link to="/owner/modify-info">
+                      내 정보
+                    </Link>
+                  </li>
+                  <li className={styles['mobile-header__link']}>
+                    <button type="button" onClick={handleLogout}>
+                      로그아웃
+                    </button>
+                  </li>
+                </ul>
               </div>
-            ))}
 
-            <img
-              className={cn({
-                [styles['mobile-header__logo']]: true,
-                [styles['mobile-header__logo--bcsd']]: true,
-              })}
-              src="http://static.koreatech.in/assets/img/ic-bcsd_gray.png"
-              alt="bcsd lab logo"
-              title="bcsd lab logo"
-            />
-            <img
-              className={cn({
-                [styles['mobile-header__logo']]: true,
-                [styles['mobile-header__logo--koin']]: true,
-              })}
-              src="http://static.koreatech.in/assets/img/rectangle_icon.png"
-              alt="KOIN service logo"
-              title="bcsd lab logo"
-            />
-          </nav>
+              {CATEGORY.map((categoryInfo) => (
+                <div key={categoryInfo.title}>
+                  <div>
+                    <div className={styles['mobile-header__category-title']}>
+                      {categoryInfo.title}
+                    </div>
+                    <ul className={styles['mobile-header__sub-menus']}>
+                      {categoryInfo.submenu.map((subMenu) => (
+                        <li
+                          className={styles['mobile-header__sub-menu']}
+                          key={subMenu.title}
+                        >
+                          <Link to={subMenu.link}>
+                            {subMenu.title === '가게정보' && subMenu.title}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+
+              <img
+                className={cn({
+                  [styles['mobile-header__logo']]: true,
+                  [styles['mobile-header__logo--bcsd']]: true,
+                })}
+                src="http://static.koreatech.in/assets/img/ic-bcsd_gray.png"
+                alt="bcsd lab logo"
+                title="bcsd lab logo"
+              />
+              <img
+                className={cn({
+                  [styles['mobile-header__logo']]: true,
+                  [styles['mobile-header__logo--koin']]: true,
+                })}
+                src="http://static.koreatech.in/assets/img/rectangle_icon.png"
+                alt="KOIN service logo"
+                title="bcsd lab logo"
+              />
+            </nav>
+          </Suspense>
         ),
         document.body,
       )}
