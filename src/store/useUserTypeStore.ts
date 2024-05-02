57b0680576b3e8a6ee80d41aa4481ebd125d841f
정해성 <gotjd0230@naver.com>
2024-05-02 17:06:37 +0900

    refactor: zustand의 persist 적용

--- src/store/useUserTypeStore.ts
+++ src/store/useUserTypeStore.ts
@@ -1,3 +1,4 @@
+import { persist } from 'zustand/middleware';
 import { isKoinError } from '@bcsdlab/koin';
 import { getUserType } from 'api/auth';
 import { UserType } from 'model/auth';
@@ -5,40 +6,58 @@ import { create } from 'zustand';
 import { useErrorMessageStore } from './errorMessageStore';
 
 interface UserTypeStore {
-  userType: UserType;
+  userType: UserType | null;
   updateUserType: () => void;
 }
 
-const useUserTypeStore = create<UserTypeStore>((set) => ({
-  userType: null,
-  updateUserType: async () => {
-    if (sessionStorage.getItem('access_token') && sessionStorage.getItem('user_type')) {
-      set((state) => {
-        if (sessionStorage.getItem('user_type') !== state.userType) {
-          return { userType: sessionStorage.getItem('user_type') as UserType };
-        }
-        return state;
-      });
-      return;
-    }
-
-    try {
-      const response = await getUserType();
-      const newUserType = response.user_type;
-      set((state) => {
-        if (newUserType !== state.userType) {
-          sessionStorage.setItem('user_type', newUserType);
-          return { userType: newUserType };
-        }
-        return state;
-      });
-    } catch (err) {
-      set((state) => (state.userType !== null ? { userType: null } : state));
-      if (isKoinError(err)) {
-        useErrorMessageStore.getState().setLoginErrorStatus(err.status);
-      }
-    }
+const sessionStorageWrapper = {
+  getItem: (name: string) => {
+    const value = sessionStorage.getItem(name);
+    return value ? JSON.parse(value) : null;
   },
-}));
+  setItem: (name: string, value: any) => {
+    sessionStorage.setItem(name, JSON.stringify(value));
+  },
+  removeItem: (name: string) => {
+    sessionStorage.removeItem(name);
+  },
+};
+
+const useUserTypeStore = create<UserTypeStore>()(
+  persist(
+    (set, get) => ({
+      userType: null,
+      updateUserType: async () => {
+        if (sessionStorage.getItem('access_token') && sessionStorage.getItem('user_type')) {
+          const userType = sessionStorage.getItem('user_type') as UserType;
+          if (userType !== get().userType) {
+            set({ userType });
+          }
+          return;
+        }
+
+        try {
+          const response = await getUserType();
+          const newUserType = response.user_type;
+          if (newUserType !== get().userType) {
+            set({ userType: newUserType });
+            sessionStorage.setItem('user_type', newUserType);
+          }
+        } catch (err) {
+          if (get().userType !== null) {
+            set({ userType: null });
+          }
+          if (isKoinError(err)) {
+            useErrorMessageStore.getState().setLoginErrorStatus(err.status);
+          }
+        }
+      },
+    }),
+    {
+      name: 'userTypeStore',
+      storage: sessionStorageWrapper,
+    },
+  ),
+);
 
 export default useUserTypeStore;
