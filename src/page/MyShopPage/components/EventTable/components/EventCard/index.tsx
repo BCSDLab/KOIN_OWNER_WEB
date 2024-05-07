aecc2f592c9f18579fdd8b335acb396c409e7044
(hoooooony) <(cjh41820@gmail.com)>
2024-05-07 17:37:16 +0900

    fix : 기본 이미지로직 변경 및 이미지 변경

--- src/page/MyShopPage/components/EventTable/components/EventCard/index.tsx
+++ src/page/MyShopPage/components/EventTable/components/EventCard/index.tsx
@@ -24,6 +24,41 @@ export default function EventCard({
       setHiddenInfo(false);
     } else setHiddenInfo(true);
   };
+  const renderThumbnail = () => {
+    if (event.thumbnail_images.length > 1) {
+      return (
+        <img
+          src={event.thumbnail_images[0]}
+          alt={event.title}
+          className={cn({
+            [styles.eventThumbail]: true,
+            [styles['eventThumbail--nonHidden']]: hiddenInfo === false,
+          })}
+        />
+      );
+    }
+    if (hiddenInfo) {
+      return (
+        <img
+          src="https://static.koreatech.in/assets/img/mainlogo2.png"
+          alt="KOIN service logo"
+          className={styles.eventThumbail}
+        />
+      );
+    }
+
+    return (
+      <div className={styles['default-event-thumbnail-img-container']}>
+        <img
+          className={styles['default-event-thumbnail-img']}
+          src="https://static.koreatech.in/assets/img/shop-event-thumbnail-default-img.png"
+          alt="이미지를 준비중 입니다."
+        />
+        <div className={styles['default-event-thumbnail-text']}>사장님이 이미지를 준비 중입니다.</div>
+      </div>
+    );
+  };
+
   return (
     <div
       className={cn({
@@ -43,25 +78,8 @@ export default function EventCard({
         {selectedEventIds.includes(event.event_id) ? (<Check />) : <NonCheck />}
       </button>
       )}
-      {event.thumbnail_images ? (
-        <img
-          src={event.thumbnail_images[0]}
-          alt={event.title}
-          className={cn({
-            [styles.eventThumbail]: true,
-            [styles['eventThumbail--nonHidden']]: hiddenInfo === false,
-          })}
-        />
-      ) : (
-        <img
-          src="https://static.koreatech.in/assets/img/empty-thumbnail.png"
-          alt="KOIN service logo"
-          className={cn({
-            [styles.eventThumbail]: true,
-            [styles['eventThumbail--nonHidden']]: hiddenInfo === false,
-          })}
-        />
-      )}
+      {renderThumbnail()}
+
       <div className={styles['event-info']}>
         <div className={styles['event-info__header']}>
           <div className={styles.title}>{event.title}</div>
