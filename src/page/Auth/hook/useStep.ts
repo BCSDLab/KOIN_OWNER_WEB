ef5abed1a73203657551da4d263d44451cd58aa0
chaeseungyun <thslr57@naver.com>
2024-06-03 00:39:49 +0900

    feat: auth 단계를 조절하는 훅 추가

---
+++ src/page/Auth/hook/useStep.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type Type = 'find' | 'register';
+
+const findPassword = [
+  '계정 인증', '비밀번호 변경',
+];
+
+const register = ['약관 동의', '기본 정보 입력', '사업자 인증'];
+
+export const useStep = (type: Type) => {
+  const target = type === 'find' ? findPassword : register;
+  const [index, setIndex] = useState(0);
+  const [isComplete, setIsComplete] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const nextStep = () => {
+    if (index + 1 < target.length) setIndex((prev) => prev + 1);
+  };
+
+  const previousStep = () => {
+    if (index > 0) setIndex((prev) => prev - 1);
+    else navigate(-1);
+  };
+
+  const currentStep = target[index];
+
+  const totalStep = target.length;
+
+  return {
+    nextStep, previousStep, currentStep, index, totalStep, isComplete, setIsComplete,
+  };
+};
