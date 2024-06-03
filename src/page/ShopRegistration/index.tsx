49d1d3cc7097e16bd64ae9ccae9c4376886327b5
Daeeui Kim <kimeodml@gmail.com>
2024-06-03 13:21:44 +0900

    refactor: 가게 관리하기 수정

--- src/page/ShopRegistration/index.tsx
+++ src/page/ShopRegistration/index.tsx
@@ -1,7 +1,13 @@
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import ShopRegistrationMobile from './view/Mobile';
+import ShopRegistrationPC from './view/PC';
 
 export default function ShopRegistration() {
+  const { isMobile } = useMediaQuery();
+
   return (
-    <ShopRegistrationMobile />
+    <div>
+      {isMobile ? <ShopRegistrationMobile /> : <ShopRegistrationPC />}
+    </div>
   );
 }
