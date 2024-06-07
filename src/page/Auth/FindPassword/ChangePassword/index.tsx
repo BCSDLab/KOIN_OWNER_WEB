e2c79eb534cea8780c3feb1e59541a8e5af47956
chaeseungyun <thslr57@naver.com>
2024-06-07 11:58:47 +0900

    feat: 비밀번호 변경 페이지 완성

---
+++ src/page/Auth/FindPassword/ChangePassword/index.tsx
@@ -0,0 +1,71 @@
+import { useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useOutletContext } from 'react-router-dom';
+import { ChangePasswordForm } from 'model/auth';
+// eslint-disable-next-line
+import { OutletProps } from 'page/Auth/FindPassword/index';
+// eslint-disable-next-line
+import styles from '../Verify/index.module.scss';
+
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,18}$/;
+
+export default function ChangePassword() {
+  const method = useFormContext<ChangePasswordForm>();
+  const {
+    register, formState: { errors, isValid }, getValues,
+  } = method;
+  const steps: OutletProps = useOutletContext();
+  const { setIsStepComplete } = steps;
+
+  useEffect(() => {
+    if (isValid) {
+      setIsStepComplete(true);
+    } else {
+      setIsStepComplete(false);
+    }
+  });
+
+  return (
+    <form className={styles.container}>
+      <section className={styles.section}>
+        <div className={styles.title}>새 비밀번호</div>
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="새 비밀번호를 입력해주세요."
+          {...register('password', {
+            required: {
+              value: true,
+              message: '필수 입력 항목입니다.',
+            },
+            pattern: {
+              value: passwordRegex,
+              message: '특수문자 포함 영어와 숫자 6~18 자리로 입력해주세요.',
+            },
+          })}
+        />
+        {errors.password
+          ? (
+            <div className={styles.error}>
+              {errors.password.message}
+            </div>
+          ) : <div className={styles.comment}>* 특수문자 포함 영어와 숫자 6~18 자리</div>}
+
+      </section>
+      <section className={styles.section}>
+        <div className={styles.title}>새 비밀번호 확인</div>
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="새 비밀번호를 다시 입력해주세요."
+          {...register('passwordCheck', {
+            validate: (value) => value === getValues('password') || '비밀번호가 일치하지 않습니다.',
+          })}
+        />
+        <div className={styles.error}>
+          {errors.passwordCheck && errors.passwordCheck.message}
+        </div>
+      </section>
+    </form>
+  );
+}
