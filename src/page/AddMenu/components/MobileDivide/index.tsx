9da209757dedb46679b68cb86f6f5d16ad42adec
daepan <qw04011@gmail.com>
2024-06-11 13:23:45 +0900

    refactor: priceInput PriceDisplay 기능 분할

--- src/page/AddMenu/components/MobileDivide/index.tsx
+++ src/page/AddMenu/components/MobileDivide/index.tsx
@@ -1,7 +1,5 @@
 import styles from './MobileDivide.module.scss';
 
 export default function MobileDivide() {
-  return (
-    <div className={styles.container}> </div>
-  );
+  return <div className={styles.container} />;
 }
