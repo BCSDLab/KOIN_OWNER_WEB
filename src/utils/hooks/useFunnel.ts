8e77f8595a28a5e75815a422f314bb4d166e1401
Daeeui Kim <kimeodml@gmail.com>
2024-06-12 18:34:08 +0900

    feat: jsDoc으로 설명 추가

--- src/utils/hooks/useFunnel.ts
+++ src/utils/hooks/useFunnel.ts
@@ -9,6 +9,11 @@ export interface FunnelProps {
   children: ReactElement<StepProps>[];
 }
 
+/**
+* 단계별 입력 폼을 진행할 시 사용 권장
+* @param { string } defaultStep 시작할 초기 단계
+* @returns { Funnel, Step, setStep, currentStep } 단계별 입력 폼을 관리하는데 사용하는 유틸리티
+*/
 export const useFunnel = (defaultStep: string) => {
   const [step, setStep] = useState(defaultStep);
 
