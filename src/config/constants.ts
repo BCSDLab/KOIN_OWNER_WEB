e466cb19983befe1cba69ff3c6bce98a478934cf
정해성 <gotjd0230@naver.com>
2024-10-11 21:09:16 +0900

    chore: 의존성 변경

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,17 @@
-import { EnvVar } from 'react-app-env';
+import { z } from 'zod';
+
+const envVar = z.object({
+  NODE_ENV: z.enum(['development', 'production']),
+  VITE_API_PATH: z.string(),
+});
+
+export type EnvVar = z.infer<typeof envVar>;
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends EnvVar { }
+  }
+}
 
 const number = (value: string) => {
   const result = Number(value);
@@ -15,17 +28,17 @@ const typeConverter = { number, string };
 function checkEnv(key: keyof EnvVar, type: 'string'): string;
 function checkEnv(key: keyof EnvVar, type: 'number'): number;
 function checkEnv(key: keyof EnvVar, type: 'number' | 'string') {
-  const value = process.env[key];
+  const value = import.meta.env[key];
   if (value !== undefined) {
     const result = typeConverter[type](value);
     if (result !== undefined) {
       return result;
     }
-    throw new Error(`process.env.${key}에 적절한 값을 설정하지 않았습니다`);
+    throw new Error(`import.meta.env.${key}에 적절한 값을 설정하지 않았습니다`);
   }
-  throw new Error(`process.env.${key}에 할당할 값이 없습니다`);
+  throw new Error(`import.meta.env.${key}에 할당할 값이 없습니다`);
 }
 
-const API_PATH = checkEnv('REACT_APP_API_PATH', 'string');
+const API_PATH = checkEnv('VITE_API_PATH', 'string');
 
 export default API_PATH;
