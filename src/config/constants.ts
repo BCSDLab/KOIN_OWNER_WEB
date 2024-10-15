b72661eed119ccb412be29ef211fda810bbc59c6
정해성 <gotjd0230@naver.com>
2024-10-15 11:52:44 +0900

    refactor: 환경변수 zod 체크 vite화

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,44 +1,19 @@
 import { z } from 'zod';
 
-const envVar = z.object({
-  NODE_ENV: z.enum(['development', 'production']),
+const Env = z.object({
   VITE_API_PATH: z.string(),
 });
 
-export type EnvVar = z.infer<typeof envVar>;
+type EnvType = z.infer<typeof Env>;
 
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends EnvVar { }
-  }
+  interface ImportMetaEnv extends EnvType { }
 }
 
-const number = (value: string) => {
-  const result = Number(value);
-  if (!Number.isNaN(result)) {
-    return result;
-  }
-  return null;
-};
-
-const string = (value: string) => value;
-
-const typeConverter = { number, string };
-
-function checkEnv(key: keyof EnvVar, type: 'string'): string;
-function checkEnv(key: keyof EnvVar, type: 'number'): number;
-function checkEnv(key: keyof EnvVar, type: 'number' | 'string') {
-  const value = import.meta.env[key];
-  if (value !== undefined) {
-    const result = typeConverter[type](value);
-    if (result !== undefined) {
-      return result;
-    }
-    throw new Error(`import.meta.env.${key}에 적절한 값을 설정하지 않았습니다`);
-  }
-  throw new Error(`import.meta.env.${key}에 할당할 값이 없습니다`);
+function checkEnv(): EnvType {
+  return Env.parse(import.meta.env);
 }
 
-const API_PATH = checkEnv('VITE_API_PATH', 'string');
+const env = checkEnv();
 
-export default API_PATH;
+export const API_PATH = env.VITE_API_PATH;
--- src/model/auth/index.ts
+++ src/model/auth/index.ts
@@ -1,4 +1,4 @@
-import z from 'zod';
+import { z } from 'zod';
 
 export const LoginParams = z.object({
   account: z.string(),
