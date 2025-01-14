1295b5d932ba7e4093f2357a3329d1882cb5b011
KimMurPin <50792467+daepan@users.noreply.github.com>
2025-01-15 00:03:44 +0900

    feat: title 관련 코드 수정 코드 수정

--- src/component/Auth/PreviousStep/index.tsx
+++ src/component/Auth/PreviousStep/index.tsx
@@ -4,9 +4,10 @@ import styles from './PreviousStep.module.scss';
 interface PreviousStepProps {
   step: number;
   clickEvent?: () => void;
+  title?: string;
 }
 
-export default function PreviousStep({ step, clickEvent }: PreviousStepProps) {
+export default function PreviousStep({ step, clickEvent, title }: PreviousStepProps) {
   // 이후에 가게 등록 첫 페이지에서 로그아웃 시 생기는 오류 해결 시 사용 예정
   // const navigate = useNavigate();
   // const handlePrevious = () => {
@@ -24,6 +25,9 @@ export default function PreviousStep({ step, clickEvent }: PreviousStepProps) {
       <button type="button" className={styles.previous__button} disabled={step === 0} onClick={clickEvent}>
         <PreviousArrowIcon title="이전 단계로 가기" />
       </button>
+      <div className={styles.previous__title}>
+        {title}
+      </div>
     </div>
   );
 }
