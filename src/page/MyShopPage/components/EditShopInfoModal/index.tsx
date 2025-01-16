0835d36004ef26ed648ae81328fe8e031d01ffbf
chaeseungyun <thslr57@naver.com>
2025-01-17 00:34:39 +0900

    fix: 가게 정보 수정 시 기본 메인 카테고리 값 추가

--- src/page/MyShopPage/components/EditShopInfoModal/index.tsx
+++ src/page/MyShopPage/components/EditShopInfoModal/index.tsx
@@ -77,7 +77,7 @@ export default function EditShopInfoModal({
         open_time: day.open_time,
         close_time: day.close_time,
       })),
-      main_category_id: shopInfo.shop_categories.map((category) => category.id)[0],
+      main_category_id: shopInfo.shop_categories.map((category) => category.id)[0] || 0,
     },
   });
 
