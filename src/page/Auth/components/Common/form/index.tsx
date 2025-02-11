b88d3dbe8bdf7d84f88a26e184ecf7efd7059933
chaeseungyun <thslr57@naver.com>
2025-02-12 01:33:53 +0900

    feat: Auth 공용 Form 컴포넌트 제작

---
+++ src/page/Auth/components/Common/form/index.tsx
@@ -0,0 +1,110 @@
+import cn from 'utils/ts/className';
+import Error from 'assets/svg/auth/error-icon.svg?react';
+import Success from 'assets/svg/auth/success.svg?react';
+import styles from './form.module.scss';
+
+interface InputProps {
+  type?: string;
+  placeholder?: string;
+  register: any;
+  name: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inputMode?: string;
+  required?: boolean;
+  pattern?: RegExp;
+  requiredMessage?: string;
+  patternMessage?: string;
+  maxLength?: number;
+  component?: React.ReactNode;
+}
+
+interface ButtonProps {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+  onSubmit?: () => void;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+interface ValidationMessageProps {
+  message?: string;
+  isError: boolean;
+}
+
+export function Title({ title }: { title: string }) {
+  return (
+    <div className={styles.title}>{title}</div>
+  );
+}
+
+export function ValidationMessage({ message, isError }: ValidationMessageProps) {
+  if (isError) {
+    return (
+      <div className={styles['message-container--error']}>
+        <Error />
+        {message}
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles['message-container--success']}>
+      {message && <Success />}
+      {message}
+    </div>
+  );
+}
+
+export function Input({
+  type = 'text', placeholder, register, onChange, inputMode, name, required, pattern,
+  requiredMessage, patternMessage, maxLength, component,
+}: InputProps) {
+  return (
+    <div className={styles['input-container']}>
+      <input
+        {...register(name, {
+          required: {
+            value: required,
+            message: requiredMessage,
+          },
+          pattern: pattern ? {
+            value: pattern,
+            message: patternMessage,
+          } : undefined,
+          onChange, // 전화번호 변경 핸들러 추가
+        })}
+        className={styles.input}
+        placeholder={placeholder}
+        type={type}
+        inputMode={inputMode}
+        maxLength={maxLength}
+      />
+      <div className={styles.timer}>
+        {component}
+      </div>
+    </div>
+  );
+}
+
+export function Button({
+  disabled = false, children, onClick, type = 'button', onSubmit,
+}: ButtonProps) {
+  return (
+    <button
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+      onClick={onClick}
+      onSubmit={onSubmit ? ((e) => {
+        e.preventDefault();
+        onSubmit();
+      }) : undefined}
+      disabled={disabled}
+      className={cn({
+        [styles.button]: true,
+        [styles.button__disabled]: disabled,
+      })}
+    >
+      {children}
+    </button>
+  );
+}
