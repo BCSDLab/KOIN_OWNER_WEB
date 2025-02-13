7e243f54ce663b29c20ffa1c17a4b744f1b8ba8d
chaeseungyun <thslr57@naver.com>
2025-02-13 14:58:52 +0900

    refactor: 불필요한 기능 삭제 및 css 조정

--- src/page/Auth/Signup/components/ownerInfoStep/index.tsx
+++ src/page/Auth/Signup/components/ownerInfoStep/index.tsx
@@ -1,5 +1,5 @@
 import { useFormContext } from 'react-hook-form';
-import { useState, useEffect, ChangeEvent } from 'react';
+import { useState } from 'react';
 import FileIcon from 'assets/svg/auth/file-icon.svg?react';
 import DeleteFile from 'assets/svg/auth/delete-file.svg?react';
 import FileUploadModal from 'page/Auth/Signup/components/fileUploadModal';
@@ -17,15 +17,8 @@ interface OwnerInfo {
   attachment_urls: { file_url: string }[];
 }
 
-interface OwnerInfoStepProps {
-  onSearch: () => void;
-  setIsStepComplete: (state: boolean) => void;
-}
-
-export default function OwnerInfoStep({ onSearch, setIsStepComplete }: OwnerInfoStepProps) {
+export default function UploadStep() {
   const {
-    register,
-    watch,
     formState: { errors },
     setValue,
   } = useFormContext<OwnerInfo>();
@@ -71,103 +64,8 @@ export default function OwnerInfoStep({ onSearch, setIsStepComplete }: OwnerInfo
     setValue('attachment_urls', newUploadedFiles);
   };
 
-  const formatCompanyNumber = (value: string) => {
-    const cleaned = value.replace(/\D/g, '');
-    const match = cleaned.match(/^(\d{0,3})(\d{0,2})(\d{0,5})$/);
-    if (match) {
-      return [match[1], match[2], match[3]].filter(Boolean).join('-');
-    }
-    return value;
-  };
-
-  const handleCompanyNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    const cleanedValue = inputValue.replace(/\D/g, '');
-    if (cleanedValue.length <= 10) {
-      const formattedValue = formatCompanyNumber(cleanedValue);
-      setValue('company_number', formattedValue);
-    }
-    if (cleanedValue.length > 10) {
-      e.target.value = cleanedValue.slice(0, 10);
-      const formattedValue = formatCompanyNumber(e.target.value);
-      setValue('company_number', formattedValue);
-    }
-  };
-
-  const watchedValues = watch(['name', 'shop_name', 'company_number', 'attachment_urls']);
-
-  useEffect(() => {
-    const values = watch();
-    const isComplete = values.name && values.shop_name
-    && values.company_number
-    && values.attachment_urls;
-    setIsStepComplete(!!isComplete);
-  }, [watchedValues, setIsStepComplete, watch]);
-
   return (
     <div className={styles['owner-info-container']}>
-      <div className={styles['owner-name']}>
-        <span className="owner-name__label">대표자명(실명)</span>
-        <input
-          {...register('name', {
-            required: {
-              value: true,
-              message: '이름을 입력해주세요',
-            },
-          })}
-          placeholder="이름을 입력해주세요."
-        />
-        {errors.name && <span className={styles['error-message']}>{errors.name.message}</span>}
-      </div>
-      <div className={styles['shop-name']}>
-        <span className="shop-name__label">가게명</span>
-        <div className={styles['shop-search-box']}>
-          <input
-            {...register('shop_name', {
-              required: {
-                value: true,
-                message: '가게명을 입력해주세요',
-              },
-              pattern: {
-                // eslint-disable-next-line no-useless-escape
-                value: /^[가-힣a-zA-Z\s\-\.\&]+$/,
-                message: '유효한 가게명을 입력해주세요',
-              },
-            })}
-            placeholder="가게명을 입력해주세요."
-            className={styles['shop-name__input']}
-            value={watch('shop_name')}
-          />
-          <button
-            type="button"
-            className={styles['search-button']}
-            onClick={() => setTimeout(onSearch, 250)}
-          >
-            가게 검색
-          </button>
-        </div>
-        {errors.shop_name && <span className={styles['error-message']}>{errors.shop_name.message}</span>}
-      </div>
-      <div className={styles['company-registration-number']}>
-        <span className="company-registration-number__label">사업자 등록번호</span>
-        <input
-          {...register('company_number', {
-            required: {
-              value: true,
-              message: '사업자 등록번호를 입력해주세요.',
-            },
-            pattern: {
-              value: /^[0-9]{3}-[0-9]{2}-[0-9]{5}$/,
-              message: '형식에 맞게 작성해주세요.',
-            },
-          })}
-          placeholder="숫자만 입력해주세요."
-          type="text"
-          inputMode="numeric"
-          onChange={handleCompanyNumberChange}
-        />
-        {errors.company_number && <span className={styles['error-message']}>{errors.company_number.message}</span>}
-      </div>
       <div className={styles['document-input']}>
         <span className="document-input__label">사업자 인증 파일</span>
         <div className={styles['document-input__condition']}>
--- src/page/Auth/Signup/components/searchShop/index.tsx
+++ src/page/Auth/Signup/components/searchShop/index.tsx
@@ -2,115 +2,109 @@ import SearchIcon from 'assets/svg/auth/search-icon.svg?react';
 import useShopList from 'query/shops';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
-import { useOutletContext } from 'react-router-dom';
 import cn from 'utils/ts/className';
-import { OutletProps } from 'page/Auth/FindPassword/entity';
+import { Button } from 'page/Auth/components/Common/form';
 import styles from './searchShop.module.scss';
 
 interface ShopInfo {
   shop_name: string;
   shop_id: number | null;
 }
-export default function SearchShop() {
-  const [searchText, setSearchText] = useState('');
-  const steps = useOutletContext<OutletProps>();
+
+export default function SearchShop({ nextStep }: { nextStep: () => void }) {
   const { shopList, isError } = useShopList();
   const {
     watch, setValue,
   } = useFormContext<ShopInfo>();
+
   function handleClickShop(e: React.MouseEvent<HTMLButtonElement>) {
     const { name, id } = JSON.parse(e.currentTarget.value);
     setValue('shop_name', name);
     setValue('shop_id', id);
-    steps.setIsShopSelect(true);
   }
 
   const [filteredShopList, setFilteredShopList] = useState(shopList?.shops);
 
-  function handleSearch() {
-    if (searchText !== '') {
-      setFilteredShopList(shopList?.shops.filter(({ name }) => name.includes(searchText)));
-    }
-  }
-
   function handleChangeSearchText(e: ChangeEvent<HTMLInputElement>) {
-    setSearchText(e.target.value);
-  }
-
-  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === 'Enter') {
-      handleSearch();
+    if (e.target.value !== '') {
+      setFilteredShopList(shopList?.shops.filter(({ name }) => name.includes(e.target.value)));
+    } else {
+      setFilteredShopList(shopList?.shops);
     }
   }
 
   useEffect(() => {
-    if (searchText === '') {
+    if (shopList?.shops) {
       setFilteredShopList(shopList?.shops);
     }
-  }, [searchText, shopList?.shops]);
+  }, [shopList]);
 
   return (
     <div className={styles['search-shop-container']}>
-      <div className={styles['search-input-container']}>
-        <input
-          type="text"
-          className={styles['search-shop__input']}
-          placeholder="가게를 검색해보세요"
-          value={searchText}
-          onChange={handleChangeSearchText}
-          onKeyDown={handleKeyDown}
-        />
-        <SearchIcon
-          type="button"
-          className={styles['search-shop__button']}
-          onClick={() => handleSearch()}
-        />
-      </div>
-      <div className={styles['shop-list-container']}>
-        {isError && <div>에러가 발생했습니다.</div>}
-        {filteredShopList?.map((shop) => (
-          <button
-            key={shop.id}
-            className={cn({
-              [styles['shop-card']]: true,
-              [styles['shop-card--selected']]: watch('shop_name') === shop.name,
-            })}
-            value={JSON.stringify({
-              name: shop.name,
-              id: shop.id,
-            })}
+      <div>
+        <div className={styles['search-input-container']}>
+          <input
+            type="text"
+            className={styles['search-shop__input']}
+            placeholder="가게를 검색해보세요"
+            onChange={handleChangeSearchText}
+          />
+          <SearchIcon
             type="button"
-            onClick={(e) => {
-              handleClickShop(e);
-            }}
-          >
-            <span className={styles['shop-card__title']}>{shop.name}</span>
-            <div className={styles['shop-card-info-container']}>
-              <span className={cn({
-                [styles['shop-card__info']]: true,
-                [styles['shop-card__info--activate']]: shop.delivery,
+            className={styles['search-shop__button']}
+          />
+        </div>
+        <div className={styles['shop-list-container']}>
+          {isError && <div>에러가 발생했습니다.</div>}
+          {filteredShopList?.map((shop) => (
+            <button
+              key={shop.id}
+              className={cn({
+                [styles['shop-card']]: true,
+                [styles['shop-card--selected']]: watch('shop_name') === shop.name,
               })}
-              >
-                배달
-              </span>
-              <span className={cn({
-                [styles['shop-card__info']]: true,
-                [styles['shop-card__info--activate']]: shop.pay_card,
+              value={JSON.stringify({
+                name: shop.name,
+                id: shop.id,
               })}
-              >
-                카드결제
-              </span>
-              <span className={cn({
-                [styles['shop-card__info']]: true,
-                [styles['shop-card__info--activate']]: shop.pay_bank,
-              })}
-              >
-                계좌이체
-              </span>
-            </div>
-          </button>
-        ))}
+              type="button"
+              onClick={(e) => {
+                handleClickShop(e);
+              }}
+            >
+              <span className={styles['shop-card__title']}>{shop.name}</span>
+              <div className={styles['shop-card-info-container']}>
+                <span className={cn({
+                  [styles['shop-card__info']]: true,
+                  [styles['shop-card__info--activate']]: shop.delivery,
+                })}
+                >
+                  배달
+                </span>
+                <span className={cn({
+                  [styles['shop-card__info']]: true,
+                  [styles['shop-card__info--activate']]: shop.pay_card,
+                })}
+                >
+                  카드결제
+                </span>
+                <span className={cn({
+                  [styles['shop-card__info']]: true,
+                  [styles['shop-card__info--activate']]: shop.pay_bank,
+                })}
+                >
+                  계좌이체
+                </span>
+              </div>
+            </button>
+          ))}
+        </div>
       </div>
+      <Button
+        onClick={nextStep}
+      >
+        다음
+      </Button>
     </div>
   );
 }
