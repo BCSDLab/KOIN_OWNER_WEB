aeb337a63f74d5ddcda43ae7bd2cccd469dc90e3
chaeseungyun <thslr57@naver.com>
2025-02-20 13:22:37 +0900

    feat: 기본 모달 제작작

--- src/page/Auth/Signup/components/Modals/commonModal/index.tsx
+++ src/page/Auth/Signup/components/Modals/commonModal/index.tsx
@@ -5,11 +5,49 @@ interface Props {
   title: string;
   onClose: () => void;
   buttonLabel: string;
+  onConfirm: () => void;
 }
 
 type CommomnProps = Partial<Props>;
 
-export default function CommonModal({ title, buttonLabel, onClose }: CommomnProps) {
+export function ConfirmModal({
+  title, onClose, buttonLabel, onConfirm,
+}: CommomnProps) {
+  return (
+    <div className={styles.overay}>
+      <div className={styles.modal}>
+        <div className={styles.title}>
+          선택
+          {' '}
+          {title}
+          {' '}
+          을(를)
+          <br />
+          <span className={styles.title__name}>삭제</span>
+          하시겠어요?
+        </div>
+        <div className={styles.button__container}>
+          <button
+            type="button"
+            onClick={onClose}
+            className={styles.button__common}
+          >
+            {buttonLabel || '취소'}
+          </button>
+          <button
+            type="button"
+            onClick={onConfirm}
+            className={styles.button__confirm}
+          >
+            {buttonLabel || '삭제하기'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function CommonModal({ title, buttonLabel, onClose }: CommomnProps) {
   return createPortal(
     <div className={styles.overay}>
       <div className={styles.modal}>
