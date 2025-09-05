0549ff0e02b3bacbe645ef23c030b56e79330404
이준영 <ff1451@gmail.com>
2025-09-05 20:10:44 +0900

    fix: lint 에러 수정

--- src/query/address.ts
+++ src/query/address.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getAddress } from 'api/address';
-import { Address } from 'model/shopInfo/address';
+import { AddressSearchResponse } from 'model/shopInfo/address';
 import { addressKeys } from './KeyFactory/addressKeys';
 
 const useAddress = (keyword: string) => {
-  const { data: addressData } = useQuery<Address>({
+  const { data: addressData } = useQuery<AddressSearchResponse>({
     queryKey: addressKeys.search(keyword),
     queryFn: () => getAddress({ keyword, currentPage: '1', countPerPage: '10' }),
     enabled: keyword.length > 0,
