53b70fc7eb9b722f0a6bc0303a7b761f94604bb2
이준영 <ff1451@gmail.com>
2025-09-05 19:25:15 +0900

    refactor: type import 수정

--- src/api/address/index.ts
+++ src/api/address/index.ts
@@ -1,5 +1,5 @@
 import { accessClient } from 'api';
-import { Address, AddressParams } from 'model/shopInfo/address';
+import { Address, type AddressParams } from 'model/shopInfo/address';
 
 export const getAddress = async (params:AddressParams) => {
   const { data } = await accessClient.get<Address>('/address/search', { params });
